01dcc8605919da2aa3d8e8339dfa821b23a949a1
MisterSynergy <mister.synergy@yahoo.com>
2023-09-27 11:06:55 +0200

    path fix

--- newDisambiguationPages.py
+++ newDisambiguationPages.py
@@ -96,7 +96,7 @@ def isDisam(item):
     return False
 
 def main():
-    db = MySQLdb.connect(host='wikidatawiki.analytics.db.svc.eqiad.wmflabs', db='wikidatawiki_p', read_default_file='~/replica.my.cnf')
+    db = MySQLdb.connect(host='wikidatawiki.analytics.db.svc.eqiad.wmflabs', db='wikidatawiki_p', read_default_file='replica.my.cnf')
     cur = db.cursor()
     db.set_character_set('utf8')
     cur.execute('SET NAMES utf8;')

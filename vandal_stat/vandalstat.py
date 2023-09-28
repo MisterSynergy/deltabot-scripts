90413be013014e0348f637898692863fdde497b1
MisterSynergy <mister.synergy@yahoo.com>
2023-09-28 09:53:15 +0200

    migrate to mariadb database connector

--- vandal_stat/vandalstat.py
+++ vandal_stat/vandalstat.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-import MySQLdb
+from os.path import expanduser
+import mariadb
 from datetime import date, timedelta
 import pywikibot
 import requests
@@ -8,7 +9,7 @@ import requests
 site = pywikibot.Site('wikidata','wikidata')
 repo = site.data_repository()
 
-db = MySQLdb.connect(host="wikidatawiki.labsdb", db="wikidatawiki_p", read_default_file="replica.my.cnf")
+db = mariadb.connect(host="wikidatawiki.labsdb", database="wikidatawiki_p", default_file=f"{expanduser('~')}/replica.my.cnf")
 
 r = requests.get('https://www.wikidata.org/w/index.php?title=Wikidata:WikiProject_Counter-Vandalism/plot1-csv&action=raw')
 csv = r.text.split('\n')
@@ -34,16 +35,33 @@ for dd in range(0,30):
     dayformat = day.strftime('%Y%m%d')
     dayformat2 = day.strftime('%Y/%m/%d')
 
-    cur = db.cursor()
-    cur.execute('SELECT COUNT(*) FROM recentchanges WHERE rc_patrolled=0 AND rc_timestamp>'+dayformat+'000000 AND rc_timestamp<='+dayformat+'235959')
+    cur = db.cursor(dictionary=True)
+    cur.execute(
+        'SELECT COUNT(*) as cnt FROM recentchanges WHERE rc_patrolled=0 AND rc_timestamp>%(dayformat_start)s AND rc_timestamp<=%(dayformat_end)s',
+        {
+            'dayformat_start' : f'{dayformat}000000',
+            'dayformat_end' : f'{dayformat}235959',
+        }
+    )
     
     for row in cur.fetchall():
-        newcsv += dayformat2 + ','+str(row[0])+',"unpatrolled edits"\n'
+        newcsv += f'{dayformat2},{row.get("cnt", 0)},"unpatrolled edits"\n'
 
-cur = db.cursor()
-cur.execute("SELECT COUNT(*) AS patrols FROM logging WHERE log_action='patrol' AND log_params LIKE '%\"6::auto\";i:0%' AND log_timestamp>"+newestformat+"000000 AND log_timestamp<="+newestformat+"235959")
+    cur.close()
+
+cur = db.cursor(dictionary=True)
+cur.execute(
+    "SELECT COUNT(*) AS patrols FROM logging WHERE log_action='patrol' AND log_params LIKE '%\"6::auto\";i:0%' AND log_timestamp>%(newestformat_start)s AND log_timestamp<=%(newestformat_end)s",
+    {
+        'newestformat_start' : f'{newestformat}000000',
+        'newestformat_end' : f'{newestformat}235959',
+    }
+)
 for row in cur.fetchall():
-    newcsv += newestformat2 + ','+str(row[0])+',"patrol actions"\n' 
+    newcsv += f'{newestformat2},{row.get("patrols", 0)},"patrol actions"\n' 
+
+cur.close()
+db.close()
 
 page = pywikibot.Page(site, 'Wikidata:WikiProject_Counter-Vandalism/plot1-csv')
 page.put(newcsv, summary='upd', minorEdit=False)

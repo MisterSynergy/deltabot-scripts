ec75145125745c11d0b0743ed4543406552abaed
MisterSynergy <mister.synergy@yahoo.com>
2023-09-28 23:29:57 +0200

    migrate to mariadb database connector; refactor

--- linked_deleted_entities/linked_deleted_entities.py
+++ linked_deleted_entities/linked_deleted_entities.py
@@ -2,42 +2,95 @@
 # -*- coding: UTF-8 -*-
 #licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import MySQLdb
+import mariadb
 import pywikibot
 import time
 import requests
-
-site = pywikibot.Site('wikidata','wikidata')
-
-header = 'Update: <onlyinclude>{0}</onlyinclude>\n{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"\n|-\n! Entity !! count !! deleted by\n'
-footer = '|}\n\n[[Category:Database reports|Deleted Wikidata entities that are still linked]]'
-table_row = '|-\n| {{{{{0}|{1}}}}} || [{{{{fullurl:Special:WhatLinksHere/{1}}}}} {2}] || [[User:{3}]]\n'
-
-query = 'SELECT pl_title, COUNT(*) FROM pagelinks LEFT JOIN page ON pl_title = page_title AND pl_namespace = page_namespace WHERE (pl_from_namespace = 0 OR pl_from_namespace = 120) AND (pl_namespace = 0 OR pl_namespace = 120)    AND page_id IS NULL GROUP BY pl_title ORDER BY COUNT(*) DESC, pl_namespace, pl_title'
+from os.path import expanduser
 
 
-def makeReport(db, query):
-    cursor = db.cursor()
-    cursor.execute(query)
+SITE = pywikibot.Site('wikidata','wikidata')
+
+HEADER = """Update: <onlyinclude>{update_timestamp}</onlyinclude>
+{{| class="wikitable sortable plainlinks" style="width:100%; margin:auto;"
+|-
+! Entity !! count !! deleted by
+"""
+
+FOOTER = """|}
+
+[[Category:Database reports|Deleted Wikidata entities that are still linked]]"""
+
+TABLE_ROW = """|-
+| {{{{{pql}|{title}}}}} || [{{{{fullurl:Special:WhatLinksHere/{title}}}}} {cnt}] || [[User:{user}]]
+"""
+
+
+def make_report() -> str:
+    db = mariadb.connect(
+        host='wikidatawiki.analytics.db.svc.wikimedia.cloud',
+        database='wikidatawiki_p',
+        default_file=f'{expanduser("~")}/replica.my.cnf',
+    )
+    cur = db.cursor(dictionary=True)
+
+    query = """SELECT
+  CONVERT(pl_title USING utf8) AS pl_title,
+  COUNT(*) AS cnt
+FROM
+  pagelinks
+    LEFT JOIN page ON pl_title=page_title AND pl_namespace=page_namespace
+WHERE
+  (pl_from_namespace=0 OR pl_from_namespace=120)
+  AND (pl_namespace=0 OR pl_namespace=120)
+  AND page_id IS NULL
+GROUP BY
+  pl_title
+ORDER BY
+  COUNT(*) DESC, pl_namespace, pl_title"""
+
+    cur.execute(query)
+
     text = ''
-    for val, cnt in cursor:
-        val = val.decode(encoding='utf-8')
-        r = requests.get('https://www.wikidata.org/w/api.php?action=query&list=logevents&leprop=user|type&letitle={0}&format=json'.format(val))
-        data = r.json()
-        user = ''
-        for m in data['query']['logevents']:
-            if m['type'] == 'delete' and m['action'] == 'delete':
-                user = m['user']
+    for row in cur.fetchall():
+        title = row.get('pl_title')
+        if title is None:
+            continue
+
+        response = requests.get(
+            url='https://www.wikidata.org/w/api.php',
+            params={
+                'action' : 'query',
+                'list' : 'logevents',
+                'leprop' : 'user|type',
+                'letitle' : title,
+                'format' : 'json',
+            }
+        )
+        data = response.json()
+        user = 'unknown'
+
+        for logevent in data.get('query', {}).get('logevents', []):
+            if logevent['type']=='delete' and logevent['action']=='delete':
+                user = logevent['user']
                 break
-        text += table_row.format(val[0], val, ('{:,}'.format(cnt)), user)
+
+        text += TABLE_ROW.format(pql=title[0], title=title, cnt=f'{row.get("cnt", 0):,}', user=user)
+
+    cur.close()
+    db.close()
+
     return text
 
-def main():
-    page = pywikibot.Page(site, 'Wikidata:Database reports/Deleted Wikidata entities that are still linked')
-    db = MySQLdb.connect(host="wikidatawiki.analytics.db.svc.eqiad.wmflabs", db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    report = makeReport(db, query)
-    text = header.format(time.strftime("%Y-%m-%d %H:%M (%Z)")) + report + footer
-    page.put(text, summary='Bot:Updating database report',minorEdit=False)
 
-if __name__ == "__main__":
+def main() -> None:
+    report = make_report()
+    text = HEADER.format(update_timestamp=time.strftime('%Y-%m-%d %H:%M (%Z)')) + report + FOOTER
+
+    page = pywikibot.Page(SITE, 'Wikidata:Database reports/Deleted Wikidata entities that are still linked')
+    page.text = text
+    page.save(summary='Bot:Updating database report', minor=False)
+
+
+if __name__=='__main__':
     main()

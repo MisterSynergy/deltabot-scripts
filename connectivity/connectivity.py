55c65b074c8b42d63d881348f0d417a13837b1aa
MisterSynergy <mister.synergy@yahoo.com>
2023-09-28 10:40:47 +0200

    migrate to mariadb database connector

--- connectivity/connectivity.py
+++ connectivity/connectivity.py
@@ -2,39 +2,42 @@
 # -*- coding: UTF-8 -*-
 # licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import MySQLdb
+import mariadb
+from os.path import expanduser
 import time
 import pywikibot
-db = MySQLdb.connect(host="wikidatawiki.analytics.db.svc.eqiad.wmflabs",
-                     db="wikidatawiki_p", read_default_file="replica.my.cnf")
-cur = db.cursor()
+
 
 text = ''
 
 
 def getData(project):
     global text
-    db = MySQLdb.connect(host="wikidatawiki.analytics.db.svc.eqiad.wmflabs",
-                         db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    cur = db.cursor()
-    cur.execute("SELECT ga, count(*) gap  FROM (SELECT COUNT(ips_item_id) AS ga, ips_site_id FROM wb_items_per_site WHERE ips_item_id IN (SELECT ips_item_id FROM wb_items_per_site WHERE ips_site_id='" +
-                project+"') GROUP BY ips_item_id) AS subquery GROUP BY ga")
+    db = mariadb.connect(host="wikidatawiki.analytics.db.svc.wikimedia.cloud",
+                         database="wikidatawiki_p", default_file=f"{expanduser('~')}/replica.my.cnf")
+    cur = db.cursor(dictionary=True)
+    cur.execute(
+        "SELECT ga, COUNT(*) AS gap FROM (SELECT COUNT(ips_item_id) AS ga, ips_site_id FROM wb_items_per_site WHERE ips_item_id IN (SELECT ips_item_id FROM wb_items_per_site WHERE ips_site_id=%(project)s) GROUP BY ips_item_id) AS subquery GROUP BY ga",
+        { 'project' : project },
+    )
     collec = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}
     total = 0
     avg = 0
     for row in cur.fetchall():
-        if row[0] == 1:
-            collec[0] += row[1]
-        elif row[0] == 2:
-            collec[1] += row[1]
-        elif row[0] <= 6:
-            collec[2] += row[1]
-        elif row[0] <= 11:
-            collec[3] += row[1]
+        if row.get('ga') == 1:
+            collec[0] += row.get('gap')
+        elif row.get('ga') == 2:
+            collec[1] += row.get('gap')
+        elif row.get('ga') <= 6:
+            collec[2] += row.get('gap')
+        elif row.get('ga') <= 11:
+            collec[3] += row.get('gap')
         else:
-            collec[4] += row[1]
-        total += row[1]
-        avg += row[1]*(row[0]-1)
+            collec[4] += row.get('gap')
+        total += row.get('gap')
+        avg += row.get('gap')*(row.get('ga')-1)
+    cur.close()
+
     if total >= 100:
         total = float(total)
         avg /= total
@@ -42,14 +45,15 @@ def getData(project):
             '{:,}'.format(int(total))+' || '+'{:4.1f}'.format(avg)
         for i in range(0, 5):
             text += ' || {:4.1f}'.format(collec[i]/total*100)+'%'
-            db = MySQLdb.connect(host="wikidatawiki.analytics.db.svc.eqiad.wmflabs",
-                                 db="wikidatawiki_p", read_default_file="replica.my.cnf")
-            cur = db.cursor()
-        cur.execute("SELECT count(*) as count, ips_site_id FROM wb_items_per_site WHERE ips_item_id IN (SELECT ips_item_id FROM wb_items_per_site WHERE ips_site_id = '" +
-                    project+"') AND ips_site_id <> '"+project+"' GROUP BY ips_site_id ORDER BY count DESC LIMIT 0,3")
+        
+        cur = db.cursor(dictionary=True)
+        cur.execute("SELECT count(*) AS count, CONVERT(ips_site_id USING utf8) AS ips_site_id FROM wb_items_per_site WHERE ips_item_id IN (SELECT ips_item_id FROM wb_items_per_site WHERE ips_site_id = %(project)s) AND ips_site_id <> %(project)s GROUP BY ips_site_id ORDER BY count DESC LIMIT 0,3")
         for row in cur.fetchall():
-            text += ' || {} || {:,}'.format(row[1].decode(), row[0])
+            text += ' || {} || {:,}'.format(row.get('ips_site_id', ''), row.get('count', 0))
         text += '\n|-'
+        cur.close()
+
+    db.close()
 
 
 def main():
@@ -58,43 +62,43 @@ def main():
     text += 'Update: <onlyinclude>' + \
         time.strftime("%Y-%m-%d %H:%M (%Z)")+'</onlyinclude>.\n\n'
     text += '== Wikipedia ==\n'+header
-    db = MySQLdb.connect(host="wikidatawiki.labsdb",
-                         db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    cur = db.cursor()
-    cur.execute("SELECT DISTINCT ips_site_id FROM wb_items_per_site WHERE ips_site_id LIKE '%wiki' AND ips_site_id <> 'commonswiki' ORDER BY ips_site_id")
+    db = mariadb.connect(host="wikidatawiki.analytics.db.svc.wikimedia.cloud",
+                         database="wikidatawiki_p", default_file=f"{expanduser('~')}/replica.my.cnf")
+    cur = db.cursor(dictionary=True)
+    cur.execute("SELECT DISTINCT CONVERT(ips_site_id USING utf8) AS ips_site_id FROM wb_items_per_site WHERE ips_site_id LIKE '%wiki' AND ips_site_id <> 'commonswiki' ORDER BY ips_site_id")
     for row in cur.fetchall():
-        getData(row[0].decode())
+        getData(row.get('ips_site_id'))
     text += '\n|}\n'
+    cur.close()
 
     text += '== Wikivoyage ==\n'+header
-    db = MySQLdb.connect(host="wikidatawiki.labsdb",
-                         db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    cur = db.cursor()
+    cur = db.cursor(dictionary=True)
     cur.execute(
-        "SELECT DISTINCT ips_site_id FROM wb_items_per_site WHERE ips_site_id LIKE '%wikivoyage' ORDER BY ips_site_id")
+        "SELECT DISTINCT CONVERT(ips_site_id USING utf8) AS ips_site_id FROM wb_items_per_site WHERE ips_site_id LIKE '%wikivoyage' ORDER BY ips_site_id")
     for row in cur.fetchall():
-        getData(row[0].decode())
+        getData(row.get('ips_site_id'))
     text += '\n|}\n'
+    cur.close()
 
     text += '== Wikisource ==\n'+header
-    db = MySQLdb.connect(host="wikidatawiki.labsdb",
-                         db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    cur = db.cursor()
+    cur = db.cursor(dictionary=True)
     cur.execute(
-        "SELECT DISTINCT ips_site_id FROM wb_items_per_site WHERE ips_site_id LIKE '%wikisource' ORDER BY ips_site_id")
+        "SELECT DISTINCT CONVERT(ips_site_id USING utf8) AS ips_site_id FROM wb_items_per_site WHERE ips_site_id LIKE '%wikisource' ORDER BY ips_site_id")
     for row in cur.fetchall():
-        getData(row[0].decode())
+        getData(row.get('ips_site_id'))
     text += '\n|}\n'
+    cur.close()
 
     text += '== Wikiquote ==\n'+header
-    db = MySQLdb.connect(host="wikidatawiki.labsdb",
-                         db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    cur = db.cursor()
+    cur = db.cursor(dictionary=True)
     cur.execute(
-        "SELECT DISTINCT ips_site_id FROM wb_items_per_site WHERE ips_site_id LIKE '%wikiquote' ORDER BY ips_site_id")
+        "SELECT DISTINCT CONVERT(ips_site_id USING utf8) AS ips_site_id FROM wb_items_per_site WHERE ips_site_id LIKE '%wikiquote' ORDER BY ips_site_id")
     for row in cur.fetchall():
-        getData(row[0].decode())
+        getData(row.get('ips_site_id'))
     text += '\n|}\n'
+    cur.close()
+    db.close()
+
     text += '\n[[Category:Wikidata statistics]]'
 
     site = pywikibot.Site('wikidata', 'wikidata')

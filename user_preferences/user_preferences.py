fb7b1d7a1f78a02b65c6796eaa32826614d14274
MisterSynergy <mister.synergy@yahoo.com>
2023-09-29 11:48:47 +0200

    refactor; migrate to mariadb

--- user_preferences/user_preferences.py
+++ user_preferences/user_preferences.py
@@ -2,48 +2,67 @@
 # -*- coding: UTF-8 -*-
 # licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import MySQLdb
+from os.path import expanduser
+from time import strftime
+
+import mariadb
 import pywikibot
-import time
-
-site = pywikibot.Site('wikidata', 'wikidata')
-
-header = 'Update: <onlyinclude>{0}</onlyinclude>\n'
-
-subheader1 = '== Gender ==\n{| class="wikitable sortable" style="width:40%;"\n|-\n! Gender !! Users\n'
-subheader2 = '== Language ==\n{| class="wikitable sortable" style="width:40%;"\n|-\n! Language !! Users\n'
-subheader3 = '== Skin ==\n{| class="wikitable sortable" style="width:40%;"\n|-\n! Skin !! Users\n'
-subfooter = '|}\n\n'
-footer = '[[Category:Wikidata statistics]]'
-
-table_row = '|-\n| {0} || {1}\n'
-
-query1 = 'SELECT up_value, count(*) FROM user_properties WHERE up_property="gender" GROUP BY up_value'
-query2 = 'SELECT up_value, count(*) FROM user_properties_anon WHERE up_property="language" GROUP BY up_value'
-query3 = 'SELECT up_value, count(*) FROM user_properties_anon WHERE up_property="skin" GROUP BY up_value'
 
 
-def makeReport(db, query):
-    cursor = db.cursor()
-    cursor.execute(query)
+HEADER = 'Update: <onlyinclude>{update_timestamp}</onlyinclude>\n'
+SUBHEADER_1 = '== Gender ==\n{| class="wikitable sortable" style="width:40%;"\n|-\n! Gender !! Users\n'
+SUBHEADER_2 = '== Language ==\n{| class="wikitable sortable" style="width:40%;"\n|-\n! Language !! Users\n'
+SUBHEADER_3 = '== Skin ==\n{| class="wikitable sortable" style="width:40%;"\n|-\n! Skin !! Users\n'
+SUBFOOTER = '|}\n\n'
+FOOTER = '[[Category:Wikidata statistics]]'
+TABLE_ROW = '|-\n| {preference} || {cnt}\n'
+
+
+def make_report(db, query) -> str:
+    cur = db.cursor(dictionary=True)
+    cur.execute(query)
+
     text = ''
-    for val, cnt in cursor:
-        text += table_row.format(val.decode(), ('{:,}'.format(cnt)))
+    for row in cur:
+        preference = row.get('up_value')
+        cnt = row.get('cnt')
+
+        if preference is None or cnt is None:
+            continue
+
+        text += TABLE_ROW.format(preference=preference, cnt=cnt)
+
+    cur.close()
+
     return text
 
 
-def main():
-    page = pywikibot.Page(site, 'Wikidata:Database reports/User preferences')
-    db = MySQLdb.connect(host="wikidatawiki.analytics.db.svc.eqiad.wmflabs",
-                         db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    report1 = makeReport(db, query1)
-    report2 = makeReport(db, query2)
-    report3 = makeReport(db, query3)
-    text = header.format(time.strftime("%Y-%m-%d %H:%M (%Z)")) + subheader1 + report1 + \
-        subfooter + subheader2 + report2 + subfooter + \
-        subheader3 + report3 + subfooter + footer
-    page.put(text, summary='Bot:Updating database report', minorEdit=False)
+def main() -> None:
+    db = mariadb.connect(
+        host='wikidatawiki.analytics.db.svc.wikimedia.cloud',
+        database='wikidatawiki_p',
+        default_file=f'{expanduser("~")}/replica.my.cnf',
+    )
+
+    query1 = 'SELECT CONVERT(up_value USING utf8) AS up_value, COUNT(*) AS cnt FROM user_properties WHERE up_property="gender" GROUP BY up_value'
+    report1 = make_report(db, query1)
+
+    query2 = 'SELECT CONVERT(up_value USING utf8) AS up_value, COUNT(*) AS cnt FROM user_properties_anon WHERE up_property="language" GROUP BY up_value'
+    report2 = make_report(db, query2)
+
+    query3 = 'SELECT CONVERT(up_value USING utf8) AS up_value, COUNT(*) AS cnt FROM user_properties_anon WHERE up_property="skin" GROUP BY up_value'
+    report3 = make_report(db, query3)
+
+    db.close()
+
+    text = HEADER.format(update_timestamp=strftime('%Y-%m-%d %H:%M (%Z)')) + SUBHEADER_1 + report1 + \
+        SUBFOOTER + SUBHEADER_2 + report2 + SUBFOOTER + \
+        SUBHEADER_3 + report3 + SUBFOOTER + FOOTER
+
+    page = pywikibot.Page(pywikibot.Site('wikidata', 'wikidata'), 'Wikidata:Database reports/User preferences')
+    page.text = text
+    page.save(summary='Bot:Updating database report', minor=False)
 
 
-if __name__ == "__main__":
+if __name__=='__main__':
     main()

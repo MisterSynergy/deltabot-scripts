2ef1d168bd2c4f48cab18aed324eaa39e66b03e9
MisterSynergy <mister.synergy@yahoo.com>
2023-09-29 11:51:24 +0200

    rewrite to fix broken report

--- userspace_items/userspaceitems.py
+++ userspace_items/userspaceitems.py
@@ -2,52 +2,117 @@
 # -*- coding: UTF-8 -*-
 #licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
+
 import MySQLdb
 import pywikibot
-import time
+from time import strftime
 
-site = pywikibot.Site('wikidata','wikidata')
 
-header = 'A list of pages with links to userspace. Update: <onlyinclude>{0}</onlyinclude>\n\n{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"\n|- style="white-space:nowrap;"\n! Item !! Link\n'
-footer = '|}\n\n[[Category:Wikidata statistics]]'
+SITE = pywikibot.Site('wikidata', 'wikidata')
+REPORT_PAGE = 'Wikidata:Database reports/User pages'
+USERSPACE_NAMES_FILE = './reports/userspace_names.dat'
 
-table_row = '|-\n| [[Q{0}]] || [[{1}:{2}:{3}]]\n'
+HEADER = f"""A list of pages with links to userspace. Update: <onlyinclude>{strftime('%Y-%m-%d %H:%M (%Z)')}</onlyinclude>
 
-query = """
-SELECT
- ips_item_id,
- ips_site_page
-FROM wb_items_per_site
-WHERE ips_site_id="{site}"
-AND ips_site_page LIKE "{ns}:%";
+{{| class="wikitable sortable plainlinks" style="width: 100%; margin: auto;"
+|- style="white-space: nowrap;"
+! Item !! Link
+"""
+FOOTER = """|}
+
+[[Category:Wikidata statistics]]"""
+TABLE_ROW = """|-
+| [[Q{qid_numerical}]] || [[{group}:{lang}:{page_title}]]
 """
 
-def makeReport(db):
+QUERY = '''SELECT
+  ips_item_id,
+  ips_site_page
+FROM
+  wb_items_per_site
+WHERE
+  ips_site_id="{site}"
+  AND ips_site_page LIKE "{ns}:%"'''
+DB_PARAMS = {
+    'host' : 'wikidatawiki.analytics.db.svc.wikimedia.cloud',
+    'db' : 'wikidatawiki_p',
+    'read_default_file' : 'replica.my.cnf',
+}
+
+WHITELIST = [  # page titles containing any of these strings are acceptable
+    'مستخدم:صندوق مستخدم/',  # arwiki
+    'Vorlage',  # de
+    'Userbox',  # en
+    'User:UBX/',  # enwiki
+    'کاربر:جعبه کاربر/',  # fawiki
+    'Wikipedysta:Userboksy/',  # plwiki
+]
+
+
+def make_report(db_conn):
     text = ''
-    f1 = open('reports/userspace_names.dat','r')
-    for line in f1:
-        row = line.strip().split('|')
-        #if row[2] == 'simple': #whitelist simple wiki
-        #    continue
-        text+= get_report(db,*row)
+    with open(USERSPACE_NAMES_FILE, mode='r', encoding='utf8') as file_handle:
+        for line in file_handle.readlines():
+            dbname, group, lang, ns = line.strip().split('|')
+            text += get_report(db_conn, dbname, group, lang, ns)
+
     return text
 
-def get_report(db, dbname, group, lang, ns):
+
+def is_userbox_template(page_title:str) -> bool:
+    for term in WHITELIST:
+        if term in page_title:
+            return True
+
+    return False
+
+
+def get_report(db_conn, dbname:str, group:str, lang:str, ns:str) -> str:
     text = ''
-    cursor = db.cursor()
-    cursor.execute(query.format(site=dbname, ns=ns))
+
+    cursor = db_conn.cursor()
+    query = QUERY.format(site=dbname, ns=ns)
+    try:
+        cursor.execute(query)
+    except MySQLdb._exceptions.ProgrammingError as exception:
+        print(exception, query)
+
     for row in cursor:
-        if 'Vorlage' in row[1] or 'Userbox' in row[1]: #whitelist userboxes
+        try:
+            qid_numerical, page_title = row
+        except:
             continue
-        text += table_row.format(row[0], group, lang, row[1])
+
+        page_title = page_title.decode('utf8')
+
+        if is_userbox_template(page_title) is True:
+            continue
+
+        text += TABLE_ROW.format(
+            qid_numerical=qid_numerical,
+            group=group,
+            lang=lang,
+            page_title=page_title
+        )
+
+    cursor.close()
+
     return text
 
-def main():
-    page = pywikibot.Page(site,'Wikidata:Database reports/User pages')
-    db = MySQLdb.connect(host="wikidatawiki.analytics.db.svc.eqiad.wmflabs",db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    report = makeReport(db)
-    text = header.format(time.strftime("%Y-%m-%d %H:%M (%Z)")) + report + footer
-    page.put(text.decode('UTF-8'),summary='Bot:Updating database report',minorEdit=False)    
-    
-if __name__ == "__main__":
+
+def main() -> None:
+    db_conn = MySQLdb.connect(**DB_PARAMS)
+    report = make_report(db_conn)
+
+    page = pywikibot.Page(SITE, REPORT_PAGE)
+    page.text = HEADER + report + FOOTER
+    page.save(
+        summary='Bot:Updating database report',
+        minor=False
+    )
+
+    db_conn.close()
+
+
+if __name__ == '__main__':
     main()

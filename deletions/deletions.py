af7954747e94e31594c288612fccd8f476ec8658
MisterSynergy <mister.synergy@yahoo.com>
2023-09-29 11:31:28 +0200

    refactor; migrate to mariadb

--- deletions/deletions.py
+++ deletions/deletions.py
@@ -2,35 +2,64 @@
 # -*- coding: UTF-8 -*-
 #licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import MySQLdb
+from os.path import expanduser
+from time import strftime
+
+import mariadb
 import pywikibot
-import time
 
-site = pywikibot.Site('wikidata', 'wikidata')
 
-header1 = '{{{{FULLPAGENAME}}/Header/{{#ifexist:{{FULLPAGENAME}}/Header/{{int:lang}}|{{int:lang}}|en}}}}\n'
-header2 = 'Update: <onlyinclude>{0}</onlyinclude>.\n\n{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"\n|-\n! {{{{int:userlogin-yourname}}}} !! {{{{int:deletionlog}}}}\n'
+HEADER_1 = '{{{{FULLPAGENAME}}/Header/{{#ifexist:{{FULLPAGENAME}}/Header/{{int:lang}}|{{int:lang}}|en}}}}\n'
+HEADER_2 = 'Update: <onlyinclude>{update_timestamp}</onlyinclude>.\n\n{{| class="wikitable sortable plainlinks" style="width:100%; margin:auto;"\n|-\n! {{{{int:userlogin-yourname}}}} !! {{{{int:deletionlog}}}}\n'
+TABLE_ROW = '|-\n| [[User:{username}|{username}]] || [{{{{fullurl:Special:Log/delete|user={username_underscore}}}}} {cnt}]\n'
+FOOTER = '|}\n\n[[Category:Wikidata statistics]]\n[[Category:Wikidata administrators]]'
 
-table_row = '|-\n| [[User:{0:s}|{0:s}]] || [{{{{fullurl:Special:Log/delete|user={1:s}}}}} {2:d}]\n'
 
-footer = '|}\n\n[[Category:Wikidata statistics]]\n[[Category:Wikidata administrators]]'
+def make_report() -> str:
+    db = mariadb.connect(
+        host='wikidatawiki.analytics.db.svc.wikimedia.cloud',
+        database='wikidatawiki_p',
+        default_file=f'{expanduser("~")}/replica.my.cnf'
+    )
+    cursor = db.cursor(dictionary=True)
 
-query1 = 'SELECT actor_name, count(*) FROM logging JOIN actor ON log_actor = actor_id WHERE log_action="delete" AND actor_user IN (SELECT ug_user FROM user_groups WHERE ug_group="sysop") GROUP BY log_actor ORDER BY actor_name'
+    query = """SELECT
+    CONVERT(actor_name USING utf8) AS actor_name,
+    COUNT(*) AS cnt
+FROM
+    logging
+        JOIN actor ON log_actor=actor_id
+WHERE
+    log_action="delete"
+    AND actor_user IN (
+        SELECT
+            ug_user
+        FROM
+            user_groups
+        WHERE
+            ug_group="sysop"
+    )
+GROUP BY
+    log_actor
+ORDER BY
+    actor_name"""
 
-def makeReport(db):
-    cursor = db.cursor()
-    cursor.execute(query1)
+    cursor.execute(query)
     text = ''
-    for user, cnt in cursor:
-        text += table_row.format(user.decode(), user.decode().replace(' ', '_'), cnt)
+    for row in cursor:
+        username = row.get('actor_name', '')
+        text += TABLE_ROW.format(username=username, username_underscore=username.replace(' ', '_'), cnt=row.get('cnt', 0))
+
     return text
 
-def main():
-    page = pywikibot.Page(site, 'Wikidata:Database reports/Deletions')
-    db = MySQLdb.connect(host="wikidatawiki.analytics.db.svc.eqiad.wmflabs", db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    report = makeReport(db)
-    text = header1 + header2.format(time.strftime("%Y-%m-%d %H:%M (%Z)")) + report + footer
-    page.put(text, summary='Bot:Updating database report', minorEdit=False)
 
-if __name__ == "__main__":
+def main():
+    text = HEADER_1 + HEADER_2.format(update_timestamp=strftime('%Y-%m-%d %H:%M (%Z)')) + make_report() + FOOTER
+
+    page = pywikibot.Page(pywikibot.Site('wikidata', 'wikidata'), 'Wikidata:Database reports/Deletions')
+    page.text = text
+    page.save(summary='Bot:Updating database report', minor=False)
+
+
+if __name__=='__main__':
     main()

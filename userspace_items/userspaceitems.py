69a7c24a38ac4f76cb6982fd32f96aeb2908fc49
MisterSynergy <mister.synergy@yahoo.com>
2023-09-29 12:02:10 +0200

    migrate to mariadb connector

--- userspace_items/userspaceitems.py
+++ userspace_items/userspaceitems.py
@@ -2,10 +2,11 @@
 # -*- coding: UTF-8 -*-
 #licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-
-import MySQLdb
-import pywikibot
+from os.path import expanduser
 from time import strftime
+
+import mariadb
+import pywikibot
 
 
 SITE = pywikibot.Site('wikidata', 'wikidata')
@@ -26,17 +27,17 @@ TABLE_ROW = """|-
 """
 
 QUERY = '''SELECT
-  ips_item_id,
-  ips_site_page
+  ips_item_id AS qid_numerical,
+  CONVERT(ips_site_page USING utf8) AS page_title
 FROM
   wb_items_per_site
 WHERE
-  ips_site_id="{site}"
-  AND ips_site_page LIKE "{ns}:%"'''
+  ips_site_id=%(dbname)s
+  AND ips_site_page LIKE %(pagename)s'''
 DB_PARAMS = {
     'host' : 'wikidatawiki.analytics.db.svc.wikimedia.cloud',
-    'db' : 'wikidatawiki_p',
-    'read_default_file' : 'replica.my.cnf',
+    'database' : 'wikidatawiki_p',
+    'default_file' : f'{expanduser("~")}/replica.my.cnf',
 }
 
 WHITELIST = [  # page titles containing any of these strings are acceptable
@@ -49,12 +50,18 @@ WHITELIST = [  # page titles containing any of these strings are acceptable
 ]
 
 
-def make_report(db_conn):
+def make_report() -> str:
+    conn = mariadb.connect(**DB_PARAMS)
+    cur = conn.cursor(dictionary=True)
+
     text = ''
     with open(USERSPACE_NAMES_FILE, mode='r', encoding='utf8') as file_handle:
         for line in file_handle.readlines():
             dbname, group, lang, ns = line.strip().split('|')
-            text += get_report(db_conn, dbname, group, lang, ns)
+            text += get_report(cur, dbname, group, lang, ns)
+
+    cur.close()
+    conn.close()
 
     return text
 
@@ -67,23 +74,21 @@ def is_userbox_template(page_title:str) -> bool:
     return False
 
 
-def get_report(db_conn, dbname:str, group:str, lang:str, ns:str) -> str:
+def get_report(cur, dbname:str, group:str, lang:str, ns:str) -> str:
     text = ''
-
-    cursor = db_conn.cursor()
-    query = QUERY.format(site=dbname, ns=ns)
+    
+    params = { 'dbname' : dbname, 'pagename' : f'{ns}:%' }
     try:
-        cursor.execute(query)
-    except MySQLdb._exceptions.ProgrammingError as exception:
-        print(exception, query)
+        cur.execute(QUERY, params)
+    except mariadb.ProgrammingError as exception:
+        print(exception, params)
 
-    for row in cursor:
-        try:
-            qid_numerical, page_title = row
-        except:
+    for row in cur:
+        qid_numerical = row.get('qid_numerical')
+        page_title = row.get('page_title')
+
+        if qid_numerical is None or page_title is None:
             continue
-
-        page_title = page_title.decode('utf8')
 
         if is_userbox_template(page_title) is True:
             continue
@@ -95,23 +100,13 @@ def get_report(db_conn, dbname:str, group:str, lang:str, ns:str) -> str:
             page_title=page_title
         )
 
-    cursor.close()
-
     return text
 
 
 def main() -> None:
-    db_conn = MySQLdb.connect(**DB_PARAMS)
-    report = make_report(db_conn)
-
     page = pywikibot.Page(SITE, REPORT_PAGE)
-    page.text = HEADER + report + FOOTER
-    page.save(
-        summary='Bot:Updating database report',
-        minor=False
-    )
-
-    db_conn.close()
+    page.text = HEADER + make_report() + FOOTER
+    page.save(summary='Bot:Updating database report', minor=False)
 
 
 if __name__ == '__main__':

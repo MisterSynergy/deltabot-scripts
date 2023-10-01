0a9ca6ebf0699b731c93d778ddf94ab1bfcc687b
MisterSynergy <mister.synergy@yahoo.com>
2023-10-01 17:23:17 +0200

    refactor; migrate to mariadb connector

--- duplicate_arts/duplicateArts.py
+++ duplicate_arts/duplicateArts.py
@@ -2,71 +2,89 @@
 # -*- coding: UTF-8 -*-
 #licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import pywikibot
+from typing import Generator
+
+import pywikibot as pwb
 import requests
-import json
 
-site = pywikibot.Site('wikidata','wikidata')
 
-def candByImage():
-    payload = {
-        'query': """SELECT DISTINCT ?item ?item2 WHERE {
-                  ?item wdt:P31/wdt:P279* wd:Q3305213 . 
-                  ?item2 wdt:P31/wdt:P279* wd:Q3305213 . 
-                  ?item wdt:P18 ?image .
-                  ?item2 wdt:P18 ?image
-                  FILTER(?item != ?item2 && str(?item) < str(?item2))
-                  } ORDER BY str(?item) LIMIT 500""",
-        'format': 'json'
-    }
-    r = requests.get('https://query.wikidata.org/bigdata/namespace/wdq/sparql?', params=payload)
-    data = r.json()
+def query_wdqs(query:str) -> Generator[tuple[str, str], None, None]:
+    response = requests.get(
+        url='https://query.wikidata.org/bigdata/namespace/wdq/sparql',
+        params={
+            query : query,
+        },
+        headers={
+            'Accept' : 'application/sparql-results+json',
+            'User-Agent': f'{requests.utils.default_user_agent()} (duplicate_arts.py via User:DeltaBot at Wikidata; mailto:tools.deltabot@toolforge.org)'
+        }
+    )
+
+    data = response.json()
+
+    for row in data.get('results', {}).get('bindings', []):
+        item = row.get('item', {}).get('value', '').replace('http://www.wikidata.org/entity/', '')
+        item2 = row.get('item2', {}).get('value', '').replace('http://www.wikidata.org/entity/', '')
+
+        yield item, item2
+
+
+def candidate_by_image() -> str:
+    query = """SELECT DISTINCT ?item ?item2 WHERE {
+  ?item wdt:P31/wdt:P279* wd:Q3305213 . 
+  ?item2 wdt:P31/wdt:P279* wd:Q3305213 . 
+  ?item wdt:P18 ?image .
+  ?item2 wdt:P18 ?image .
+  FILTER(?item != ?item2 && str(?item) < str(?item2)) .
+} ORDER BY str(?item) LIMIT 500"""
+
     text = ''
-    for m in data['results']['bindings']:
-        item = m['item']['value'].replace('http://www.wikidata.org/entity/', '')
-        item2 = m['item2']['value'].replace('http://www.wikidata.org/entity/', '')
-        text += '*{{Q|'+item+'}}, {{Q|'+item2+'}}\n'
+    for item, item2 in query_wdqs(query):
+        text += f'* {{{{Q|{item}}}}}, {{{{Q|{item2}}}}}\n'
+
     return text
 
 
-def candByQualifier(ps, pq):
-    payload = {
-        'query': """SELECT DISTINCT ?item1 ?item2 WHERE {{
-                  ?item1 wdt:P31/wdt:P279* wd:Q3305213 .   
-                  ?item1 p:{0} ?statement1 .
-                  ?statement1 ps:{0} ?value;
-                              pq:{1} ?qvalue1 .
-                  ?item2 wdt:P31/wdt:P279* wd:Q3305213 .   
-                  ?item2 p:{0} ?statement2 .
-                  ?statement2 ps:{0} ?value;
-                              pq:{1} ?qvalue2 .
-                  FILTER(str(?item1) < str(?item2))
-                  FILTER(?qvalue1 = ?qvalue2)
-                  }} ORDER BY str(?item1) LIMIT 500
-                """.format(ps, pq),
-        'format': 'json'
-    }
-    r = requests.get('https://query.wikidata.org/bigdata/namespace/wdq/sparql?', params=payload)
-    data = r.json()
+def candidate_by_qualifier(ps:str, pq:str) -> str:
+    query = f"""SELECT DISTINCT ?item1 ?item2 WHERE {{
+  ?item1 wdt:P31/wdt:P279* wd:Q3305213 .   
+  ?item1 p:{ps} ?statement1 .
+  ?statement1 ps:{ps} ?value; pq:{pq} ?qvalue1 .
+
+  ?item2 wdt:P31/wdt:P279* wd:Q3305213 .   
+  ?item2 p:{ps} ?statement2 .
+  ?statement2 ps:{ps} ?value; pq:{pq} ?qvalue2 .
+
+  FILTER(STR(?item1) < STR(?item2)) .
+  FILTER(?qvalue1 = ?qvalue2) .
+}} ORDER BY STR(?item1) LIMIT 500"""
+
     text = ''
-    for m in data['results']['bindings']:
-        item1 = m['item1']['value'].replace('http://www.wikidata.org/entity/', '')
-        item2 = m['item2']['value'].replace('http://www.wikidata.org/entity/', '')
-        text += '*{{Q|'+item1+'}}, {{Q|'+item2+'}}\n'
+    for item1, item2 in query_wdqs(query):
+        text += f'*{{{{Q|{item1}}}}}, {{{{Q|{item2}}}}}\n'
+
     return text
 
 
-def main():
-    mtext = '__TOC__\n== Items with same inventory number of the same collection ==\n\n'
-    mtext += candByQualifier('P217', 'P195')
-    mtext += '\n== Items with same catalog code of the same catalog ==\n\n'
-    mtext += candByQualifier('P528', 'P972')    
-    mtext += '\n== Paintings with same image ==\n\n'
-    mtext += candByImage()
-    mtext += '\n[[Category:WikiProject sum of all paintings|Duplicate paintings]]\n[[Category:Database reports|Duplicate paintings]]\n[[Category:Merge candidates|Duplicate paintings]]'
+def main() -> None:
+    text = f"""__TOC__
+== Items with same inventory number of the same collection ==
+{candidate_by_qualifier('P217', 'P195')}
 
-    page = pywikibot.Page(site, 'Wikidata:WikiProject sum of all paintings/Duplicate paintings')
-    page.put(mtext, summary='upd', minorEdit=False)
+== Items with same catalog code of the same catalog ==
+{candidate_by_qualifier('P528', 'P972')}
 
-if __name__ == "__main__":
+== Paintings with same image ==
+{candidate_by_image()}
+
+[[Category:WikiProject sum of all paintings|Duplicate paintings]]
+[[Category:Database reports|Duplicate paintings]]
+[[Category:Merge candidates|Duplicate paintings]]"""
+
+    page = pwb.Page(pwb.Site('wikidata', 'wikidata'), 'Wikidata:WikiProject sum of all paintings/Duplicate paintings')
+    page.text = text
+    page.save(summary='upd', minor=False)
+
+
+if __name__ == '__main__':
     main()

0d77cd6b6b979d8adabc54cc814547159d9e3605
MisterSynergy <mister.synergy@yahoo.com>
2023-10-02 09:53:17 +0200

    refactor; migrate to mariadb connector

--- popular_without_claims/popular_withoutclaims.py
+++ popular_without_claims/popular_withoutclaims.py
@@ -2,51 +2,109 @@
 # -*- coding: UTF-8 -*-
 # licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import MySQLdb
-import pywikibot
-import time
+from os.path import expanduser
+from time import strftime
 
-site = pywikibot.Site('wikidata', 'wikidata')
+import mariadb
+import pywikibot as pwb
 
-header = 'Update: <onlyinclude>{0}</onlyinclude>\n==Items with the most links but without statements==\n{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"\n|-\n! Item !! # links\n'
-middle = '|}\n\n==Items with the most sitelinks but without statements==\n{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"\n|-\n! Item !! # sitelinks\n'
-footer = '|}\n\n[[Category:Database reports|Popular items without claims]]'
-table_row = '|-\n| {{{{Q|{0}}}}} || {1}\n'
 
-query1 = """SELECT pl_title, count(*) AS cnt FROM pagelinks
-    WHERE pl_from_namespace=0 AND pl_namespace=0
+HEADER = """Update: <onlyinclude>{update_timestamp}</onlyinclude>
+==Items with the most links but without statements==
+{{| class="wikitable sortable plainlinks" style="width:100%; margin:auto;"
+|-
+! Item !! # links
+"""
+MIDDLE = """|}
+
+==Items with the most sitelinks but without statements==
+{| class="wikitable sortable plainlinks" style="width:100%; margin:auto;"
+|-
+! Item !! # sitelinks
+"""
+FOOTER = """|}
+
+[[Category:Database reports|Popular items without claims]]"""
+TABLE_ROW = """|-
+| {{{{Q|{qid}}}}} || {cnt}
+"""
+
+QUERY_1 = """SELECT
+    CONVERT(pl_title USING utf8) AS qid,
+    COUNT(*) AS cnt
+FROM
+    pagelinks
+WHERE
+    pl_from_namespace=0
+    AND pl_namespace=0
     AND pl_title IN (
-        SELECT page_title FROM page JOIN page_props ON page_id=pp_page
-        WHERE pp_propname = "wb-claims" AND pp_value=0
+        SELECT
+            page_title
+        FROM
+            page
+                JOIN page_props ON page_id=pp_page
+        WHERE
+            pp_propname='wb-claims'
+            AND pp_value=0
     )
-    GROUP BY pl_title
-    ORDER BY cnt DESC
-    LIMIT 100;"""
+GROUP BY
+    pl_title
+ORDER BY
+    cnt DESC
+LIMIT
+    100"""
 
-query2 = """SELECT page_title, ppsitelinks.pp_value FROM page_props AS ppclaims
-    JOIN page ON ppclaims.pp_page=page_id AND page_namespace=0 AND page_is_redirect=0
-    JOIN page_props AS ppsitelinks ON page_id=ppsitelinks.pp_page AND ppsitelinks.pp_propname='wb-sitelinks'
-    WHERE ppclaims.pp_propname='wb-claims' AND ppclaims.pp_value=0
-    ORDER BY CAST(ppsitelinks.pp_value AS int) DESC
-    LIMIT 100;"""
-  
+QUERY_2 = """SELECT
+    CONVERT(page_title USING utf8) AS qid,
+    ppsitelinks.pp_value AS cnt
+FROM
+    page_props AS ppclaims
+        JOIN page ON ppclaims.pp_page=page_id AND page_namespace=0 AND page_is_redirect=0
+        JOIN page_props AS ppsitelinks ON page_id=ppsitelinks.pp_page AND ppsitelinks.pp_propname='wb-sitelinks'
+WHERE
+    ppclaims.pp_propname='wb-claims'
+    AND ppclaims.pp_value=0
+ORDER BY
+    CAST(ppsitelinks.pp_value AS int) DESC
+LIMIT
+    100"""
 
-def makeReport(db, query):
-    cursor = db.cursor()
+
+def make_report(db, query) -> str:
+    cursor = db.cursor(dictionary=True)
     cursor.execute(query)
+
     text = ''
-    for val, cnt in cursor:
-        text += table_row.format(val.decode(), cnt)
+    for row in cursor:
+        qid = row.get('qid')
+        cnt = row.get('cnt')
+
+        if qid is None or cnt is None:
+            continue
+
+        text += TABLE_ROW.format(qid=qid, cnt=cnt)
+
     return text
 
 
-def main():
-    page = pywikibot.Page(site, 'Wikidata:Database reports/Popular items without claims')
-    db = MySQLdb.connect(host="wikidatawiki.analytics.db.svc.eqiad.wmflabs", db="wikidatawiki_p", read_default_file="replica.my.cnf")
-    report1 = makeReport(db, query1)
-    report2 = makeReport(db, query2)
-    text = header.format(time.strftime("%Y-%m-%d %H:%M (%Z)")) + report1 + middle + report2 + footer
-    page.put(text, summary='Bot:Updating database report', minorEdit=False)
+def main() -> None:
+    db = mariadb.connect(
+        host='wikidatawiki.analytics.db.svc.eqiad.wmflabs',
+        database='wikidatawiki_p',
+        default_file=f'{expanduser("~")}/replica.my.cnf',
+    )
 
-if __name__ == "__main__":
+    report1 = make_report(db, QUERY_1)
+    report2 = make_report(db, QUERY_2)
+
+    db.close()
+
+    text = HEADER.format(update_timestamp=strftime('%Y-%m-%d %H:%M (%Z)')) + report1 + MIDDLE + report2 + FOOTER
+  
+    page = pwb.Page(pwb.Site('wikidata', 'wikidata'), 'Wikidata:Database reports/Popular items without claims')
+    page.text = text
+    page.save(summary='Bot:Updating database report', minor=False)
+
+
+if __name__=='__main__':
     main()

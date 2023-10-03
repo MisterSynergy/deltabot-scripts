2309a5f74217cd1b199245fc575664a2f8c10cbb
MisterSynergy <mister.synergy@yahoo.com>
2023-10-03 21:26:54 +0200

    refactor

--- incomplete_mergers/incomplete_mergers.py
+++ incomplete_mergers/incomplete_mergers.py
@@ -2,77 +2,71 @@
 # -*- coding: UTF-8 -*-
 # licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-from os.path import expanduser
-import pywikibot
+import pywikibot as pwb
 from pywikibot.data import api
 from datetime import datetime, timedelta
 import re
 
-site = pywikibot.Site('wikidata', 'wikidata')
-site.login()
-repo = site.data_repository()
+
+SITE = pwb.Site('wikidata', 'wikidata')
+REPO = SITE.data_repository()
+
+TIME_FILE = './jobs/incomplete_mergers/incomplete_mergers_time.dat'
 
 
-def addition_check(q1, q2):
+def addition_check(qid_1:str, qid_2:str) -> bool:
     # only pass if not linked together
-    params2 = {
+    params = {
         'action': 'query',
-        'titles': q1,
+        'titles': qid_1,
         'prop': 'links',
         'pllimit': 500
     }
-    req2 = api.Request(site=site, parameters=params2)
-    data2 = req2.submit()
-    for m in data2['query']['pages']:
-        try:
-            for m in data2['query']['pages'][m]['links']:
-                if q2 == m['title']:
-                    return False
-        except:
-            pass
+    api_request = api.Request(site=SITE, parameters=params)
+    data = api_request.submit()
+    for page_id in data.get('query', {}).get('pages', []):
+        for page in data.get('query', {}).get('pages', {}).get(page_id, {}).get('links', []):
+            if qid_2 == page.get('title', ''):
+                return False
 
     # check if q1 has wikimedia in its description but not q2
-    item1 = pywikibot.ItemPage(repo, q1)
-    item2 = pywikibot.ItemPage(repo, q2)
-    item1.get()
-    item2.get()
-    if 'en' in item1.descriptions and 'en' in item2.descriptions:
-        if 'ikimedia' in item1.descriptions['en'] and 'ikimedia' not in item2.descriptions['en']:
+    item_1 = pwb.ItemPage(REPO, qid_1)
+    item_1.get()
+
+    item_2 = pwb.ItemPage(REPO, qid_2)
+    item_2.get()
+
+    if 'en' in item_1.descriptions and 'en' in item_2.descriptions:
+        if 'ikimedia' in item_1.descriptions.get('en', '') and 'ikimedia' not in item_2.descriptions.get('en', ''):
             return False
-
-    dab_items = [ 'Q4167410', 'Q15407973', 'Q22808320', 'Q61996773', 'Q66480449' ]
-    item1_is_dab, item2_is_dab = False, False
-    for claim in item1.claims.get('P31', []):
-        if claim.getTarget().title() in dab_items:
-            item1_is_dab = True
-    for claim in item2.claims.get('P31', []):
-        if claim.getTarget().title() in dab_items:
-            item2_is_dab = True
-
-    if item1_is_dab and not item2_is_dab:
-        return False
 
     return True
 
 
-def merge(fromId, toId):
-    if addition_check(fromId, toId) == False:
-        return 0
-    fromItem = pywikibot.ItemPage(repo, fromId)
-    toItem = pywikibot.ItemPage(repo, toId)
-    fromItem.mergeInto(toItem, ignore_conflicts='description')
-    if not fromItem.isRedirectPage():
-        clearItem(fromId)
-        fromItem.set_redirect_target(toItem, force=True, save=True)
+def merge(from_qid:str, to_qid:str) -> None:
+    if addition_check(from_qid, to_qid) is False:
+        return
+
+    from_item = pwb.ItemPage(REPO, from_qid)
+    to_item = pwb.ItemPage(REPO, to_qid)
+
+    try:
+        from_item.mergeInto(to_item, ignore_conflicts='description')
+    except:
+        pass
+
+    if not from_item.isRedirectPage():
+        clear_item(from_qid)
+        from_item.set_redirect_target(to_item, force=True, save=True)
 
 
-def clearItem(fromId):
+def clear_item(fromId):
     # get token
     params = {
         'action': 'query',
         'meta': 'tokens'
     }
-    req = api.Request(site=site, parameters=params)
+    req = api.Request(site=SITE, parameters=params)  # upd 2022-12-05 by User:MisterSynergy from **params to parameters=params
     data = req.submit()
     # clear item
     params2 = {
@@ -84,101 +78,105 @@ def clearItem(fromId):
         'summary': 'clearing item to prepare for redirect',
         'token': data['query']['tokens']['csrftoken']
     }
-    req2 = api.Request(site=site, parameters=params2)
+    req2 = api.Request(site=SITE, parameters=params2)
     data2 = req2.submit()
 
 
-def check(q1):
-    item = pywikibot.ItemPage(repo, q1)
-    # ignore redirects and deleted items
-    if item.isRedirectPage():
-        return 0
+def check(qid:str) -> None:
+    item = pwb.ItemPage(REPO, qid)
+
     if not item.exists():
-        return 0
+        return
+    if item.isRedirectPage():
+        return
+
     # ignore items with sitelinks and more than 3 claims
-    dict = item.get()
-    if len(dict['sitelinks']) != 0:
-        return 0
-    if len(dict['claims']) > 3:
-        return 0
+    item_json = item.get()
+    if len(item_json.get('sitelinks', [])) > 0:
+        return
+    if len(item_json.get('claims', [])) > 3:
+        return
+
     # ignore items with more than 10 backlinks
-    params2 = {
+    params = {
         'action': 'query',
         'list': 'backlinks',
-        'bltitle': q1,
+        'bltitle': qid,
         'blnamespace': 0,
         'bllimit': 11
     }
-    req2 = api.Request(site=site, parameters=params2)
-    data2 = req2.submit()
-    if len(data2['query']['backlinks']) > 10:
-        return 0
+    api_request = api.Request(site=SITE, parameters=params)
+    data = api_request.submit()
+    if len(data.get('query', {}).get('backlinks', [])) > 10:
+        return
+
     # ignore items with link to WD namespace
-    params2 = {
+    params = {
         'action': 'query',
         'list': 'backlinks',
-        'bltitle': q1,
+        'bltitle': qid,
         'blnamespace': 4,
         'bllimit': 1
     }
-    req2 = api.Request(site=site, parameters=params2)
-    data2 = req2.submit()
-    if len(data2['query']['backlinks']) > 0:
-        return 0
+    api_request = api.Request(site=SITE, parameters=params)
+    data = api_request.submit()
+    if len(data.get('query', {}).get('backlinks', [])) > 0:
+        return
+
     # ignore items in Category:Notability policy exemptions
-    cat = pywikibot.Category(site, 'Category:Notability policy exemptions')
-    liste = list(cat.articles(recurse=5))
-    for m in liste:
-        if q1 == m.title()[5:]:
-            return 0
+    cat = pwb.Category(SITE, 'Category:Notability policy exemptions')
+    category_members = list(cat.articles(recurse=5))
+    for page in category_members:
+        if qid==page.title()[5:]:  # TODO: why [5:]?
+            return
+
     # ignore items where all removed sitelinks are not added to the same new item
-    params1 = {
+    params = {
         'action': 'query',
         'prop': 'revisions',
-        'titles': q1,
+        'titles': qid,
         'rvlimit': 200
     }
-    req1 = api.Request(site=site, parameters=params1)
-    data1 = req1.submit()
-    q2 = False
-    for foo in data1['query']['pages']:
-        for m in data1['query']['pages'][foo]['revisions']:
-            res = re.search(
-                'wbsetsitelink-remove\:1\|(.*)wiki \*\/ (.*)', m['comment'])
-            if res:
-                try:
-                    site2 = pywikibot.Site(
-                        res.group(1).replace('_', '-'), "wikipedia")
-                    page = pywikibot.Page(site2, res.group(2))
-                    item2 = pywikibot.ItemPage.fromPage(page)
-                    if item2.exists():
-                        qTem = item2.getID()
-                        if not q2 or q2 == qTem:
-                            q2 = qTem
-                        else:
-                            return 0
-                except:
-                    return 0
-    if q2:
-        try:
-            merge(q1, q2)
-        except:
-            pass
-        return 1
+    api_request = api.Request(site=SITE, parameters=params)
+    data = api_request.submit()
+    qid_other = None
+
+    for page_id in data.get('query', {}).get('pages', []):
+        for revision in data.get('query', {}).get('pages', {}).get(page_id, {}).get('revisions', []):
+            matches = re.search('wbsetsitelink-remove\:1\|(.*)wiki \*\/ (.*)', revision.get('comment', ''))
+            if not matches:
+                continue
+
+            try:
+                site_wikipedia = pwb.Site(matches.group(1).replace('_', '-'), 'wikipedia')
+                page = pwb.Page(site_wikipedia, matches.group(2))
+                item_other = pwb.ItemPage.fromPage(page)
+            except:
+                continue
+
+            if not item_other.exists():
+                continue
+
+            qid_candidate = item_other.getID()
+            if not qid_other or qid_other == qid_candidate:
+                qid_other = qid_candidate
+            else:
+                continue
+
+    if qid_other is None:
+        return
+
+    merge(qid, qid_other)
 
 
 def main():
-    time_file = f'{expanduser("~")}/jobs/incomplete_mergers/incomplete_mergers_time.dat'
-    with open(time_file, 'r') as file_handle:
-        done_until_str = file_handle.read()
+    with open(TIME_FILE, mode='r', encoding='utf8') as file_handle:
+        old_time_str = str(int(file_handle.read())+1)
 
-    old_time_str = (datetime.strptime(done_until_str, "%Y%m%d%H%M%S") \
-        + timedelta(seconds=1)).strftime("%Y%m%d%H%M%S")
+    new_time = datetime.now() - timedelta(minutes=10)
+    new_time_str = new_time.strftime('%Y%m%d%H%M%S')
 
-    now = datetime.now()
-    new_time = now - timedelta(minutes=10)
-    new_time_str = new_time.strftime("%Y%m%d%H%M%S")
-    rccontinue = old_time_str+'|0'
+    rccontinue = None
     while True:
         params = {
             'action': 'query',
@@ -193,23 +191,30 @@ def main():
             'rclimit': 500,
             'rccontinue': rccontinue
         }
-        req = api.Request(site=site, parameters=params)
-        data = req.submit()
-        for m in data['query']['recentchanges']:
-            timestamp = m['timestamp']
-            if 'comment' not in m:
+        if rccontinue is None:  # remove parameter for first request
+            params.pop('rccontinue')
+
+        api_request = api.Request(site=SITE, parameters=params)
+        data = api_request.submit()
+        for revision in data.get('query', {}).get('recentchanges', {}):
+            timestamp = revision.get('timestamp', '')
+            if 'comment' not in revision:
                 continue
-            res = re.search(
-                'wbsetsitelink-remove\:1\|(.*)wiki \*\/ (.*)', m['comment'])
-            if res:
-                check(m['title'])
-        if 'query-continue' in data:
-            rccontinue = data['query-continue']['recentchanges']['rccontinue']
-        else:
+
+            matches = re.search('wbsetsitelink-remove\:1\|(.*)wiki \*\/ (.*)', revision.get('comment', ''))
+            if not matches:
+                continue
+
+            check(revision.get('title'))
+
+        if 'query-continue' not in data:
             break
-    with open(time_file, 'w') as file_handle:
+
+        rccontinue = data.get('query-continue', {}).get('recentchanges', {}).get('rccontinue', '')
+
+    with open(TIME_FILE, mode='w', encoding='utf8') as file_handle:
         file_handle.write(re.sub(r'\:|\-|Z|T', '', timestamp))
 
 
-if __name__ == "__main__":
+if __name__=='__main__':
     main()

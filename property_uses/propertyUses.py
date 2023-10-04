b112f77d032a70624c20204f90e0f31258474849
MisterSynergy <mister.synergy@yahoo.com>
2023-10-04 12:17:04 +0200

    refactor

--- property_uses/propertyUses.py
+++ property_uses/propertyUses.py
@@ -2,118 +2,181 @@
 # -*- coding: UTF-8 -*-
 # licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import pywikibot
+from json import JSONDecodeError
+from time import sleep, strftime
+
+import pywikibot as pwb
 import requests
-import time
+from requests.utils import default_user_agent
 
-site = pywikibot.Site("wikidata", "wikidata")
-repo = site.data_repository()
+SITE = pwb.Site('wikidata', 'wikidata')
 
-total = {}
-mainsnak = {}
-qualifiers = {}
-references = {}
+LDF_ENDPOINT = 'https://query.wikidata.org/bigdata/ldf'
+LDF_USER_AGENT =f'{default_user_agent()} (property_uses.py via User:DeltaBot at Wikidata; mailto:tools.deltabot@toolforge.org)'
+LDF_SLEEP = 1  # seconds between requests, in order to avoid being blocked at the endpoint
 
-# collect data
-apcontinue = ""
-while True:
-    payload = {
-        "action": "query",
-        "list": "allpages",
-        "apnamespace": 120,
-        "aplimit": "max",
-        "apcontinue": apcontinue,
-        "format": "json",
-    }
-    r = requests.get("https://www.wikidata.org/w/api.php", params=payload)
-    data = r.json()
-    for m in data["query"]["allpages"]:
-        try:
-            property = m["title"][9:]
-            r2 = requests.get(
-                "https://plnode.toolforge.org/triplecount?query=SELECT ?a WHERE {{?a p:{:s} ?o}}".format(
-                    property
-                )
-            )
-            data2 = r2.json()
-            total[property] = data2["count"]
-            mainsnak[property] = data2["count"]
-            r2 = requests.get(
-                "https://plnode.toolforge.org/triplecount?query=PREFIX pq: <http://www.wikidata.org/prop/qualifier/> SELECT ?a WHERE {{?a pq:{:s} ?o}}".format(
-                    property
-                )
-            )
-            data2 = r2.json()
-            total[property] += data2["count"]
-            qualifiers[property] = data2["count"]
-            r2 = requests.get(
-                "https://plnode.toolforge.org/triplecount?query=PREFIX pr: <http://www.wikidata.org/prop/reference/> SELECT ?a WHERE {{?a pr:{:s} ?o}}".format(
-                    property
-                )
-            )
-            data2 = r2.json()
-            total[property] += data2["count"]
-            references[property] = data2["count"]
-        except Exception as e:
-            print(e)
 
-    if "continue" in data:
-        apcontinue = data["continue"]["apcontinue"]
-    else:
-        break
+def query_uses(predicate:str) -> int:
+    response = requests.get(
+        url=LDF_ENDPOINT,
+        params={
+            'predicate' : predicate,
+        },
+        headers={
+            'User-Agent' : LDF_USER_AGENT,
+            'Accept' : 'application/ld+json',
+        }
+    )
+    sleep(LDF_SLEEP)
+
+    try:
+        data = response.json()
+    except JSONDecodeError as exception:
+        if response.status_code == 429:  # we are likely running too fast
+            sleep(60)
+        print(response.text)
+        raise RuntimeError('Cannot parse LDF endpoint response body as JSON') from exception
+
+    for dct in data.get('@graph', []):
+        if 'void:triples' not in dct:
+            continue
+
+        return int(dct['void:triples'])
+
+    raise RuntimeError('Not triple count found in JSON response')
+
+
+def query_mainsnak_uses(prop:str) -> int:
+    return query_uses(f'p:{prop}')
+
+
+def query_qualifier_uses(prop:str) -> int:
+    return query_uses(f'pq:{prop}')
+
+
+def query_reference_uses(prop:str) -> int:
+    return query_uses(f'pr:{prop}')
+
+
+def collect_data() -> tuple[dict[str, int], dict[str, int], dict[str, int], dict[str, int]]:
+    total:dict[str, int] = {}
+    mainsnak:dict[str, int] = {}
+    qualifiers:dict[str, int] = {}
+    references:dict[str, int] = {}
+
+    # collect data
+    apcontinue = ''
+    while True:
+        payload = {
+            'action' : 'query',
+            'list' : 'allpages',
+            'apnamespace' : '120',
+            'aplimit' : 'max',
+            'apcontinue' : apcontinue,
+            'format' : 'json',
+        }
+        response = requests.get('https://www.wikidata.org/w/api.php', params=payload)
+        data = response.json()
+        for m in data.get('query', {}).get('allpages', {}):
+            prop = m.get('title', '')[len('Property:'):]
+
+            mainsnak_count = query_mainsnak_uses(prop)
+            total[prop] = mainsnak_count
+            mainsnak[prop] = mainsnak_count
+
+            qualifier_count = query_qualifier_uses(prop)
+            total[prop] += qualifier_count
+            qualifiers[prop] = qualifier_count
+
+            reference_count = query_reference_uses(prop)
+            total[prop] += reference_count
+            references[prop] = reference_count
+
+            #print(strftime('%Y-%m-%d, %H:%M:%S'), prop, mainsnak_count, qualifier_count, reference_count, total[prop])
+
+        if 'continue' not in data:
+            break
+
+        apcontinue = data.get('continue', {}).get('apcontinue', '')
+
+    return total, mainsnak, qualifiers, references
+
+
+def save_to_wiki_page(page_title:str, wikitext:str) -> None:
+    page = pwb.Page(SITE, page_title)
+    page.text = wikitext
+    page.save(summary='upd', minor=False)
+
+
+def write_report(dct:dict[str, int], page_title:str) -> None:
+    wikitext = '{{#switch:{{{1}}}\n'
+    keys = list(dct.keys())
+    keys.sort(key=lambda x: int(x[1:]))
+    for pid in keys:
+        wikitext += f'{pid[1:]}={dct[pid]:d}\n'
+    wikitext += '}}\n'
+    wikitext += '<noinclude>{{Documentation}}</noinclude>'
+
+    save_to_wiki_page(page_title, wikitext)
+
 
 # write [[Template:Property uses]]
-text = "<includeonly>{{#switch:{{{1}}}\n"
-keys = list(total.keys())
-keys.sort(key=lambda x: int(x[1:]))
-for p in keys:
-    text += "|{}={}\n".format(p[1:], str(total[p]))
-text += "}}</includeonly>\n"
-text += "<noinclude>{{Documentation}}</noinclude>"
-page = pywikibot.Page(site, "Template:Property uses")
-page.put(text, summary="upd", minorEdit=False)
+def write_property_uses_template(dct:dict[str, int]) -> None:
+    write_report(dct, 'Template:Property uses')
+
 
 # write [[Template:Number of main statements by property]]
-text = "<includeonly>{{#switch:{{{1}}}\n"
-keys = list(mainsnak.keys())
-keys.sort(key=lambda x: int(x[1:]))
-for p in keys:
-    text += "|{}={}\n".format(p[1:], str(mainsnak[p]))
-text += "}}</includeonly>\n"
-text += "<noinclude>{{Documentation}}</noinclude>"
-page = pywikibot.Page(site, "Template:Number of main statements by property")
-page.put(text, summary="upd", minorEdit=False)
+def write_number_of_main_statements_by_property_template(dct:dict[str, int]) -> None:
+    write_report(dct, 'Template:Number of main statements by property')
+
 
 # write [[Template:Number of qualifiers by property]]
-text = "<includeonly>{{#switch:{{{1}}}\n"
-keys = list(qualifiers.keys())
-keys.sort(key=lambda x: int(x[1:]))
-for p in keys:
-    text += "|{}={}\n".format(p[1:], str(qualifiers[p]))
-text += "}}</includeonly>\n"
-text += "<noinclude>{{Documentation}}</noinclude>"
-page = pywikibot.Page(site, "Template:Number of qualifiers by property")
-page.put(text, summary="upd", minorEdit=False)
+def write_number_of_qualifiers_by_property_template(dct:dict[str, int]) -> None:
+    write_report(dct, 'Template:Number of qualifiers by property')
+
 
 # write [[Template:Number of references by property]]
-text = "<includeonly>{{#switch:{{{1}}}\n"
-keys = list(references.keys())
-keys.sort(key=lambda x: int(x[1:]))
-for p in keys:
-    text += "|{}={}\n".format(p[1:], str(references[p]))
-text += "}}</includeonly>\n"
-text += "<noinclude>{{Documentation}}</noinclude>"
-page = pywikibot.Page(site, "Template:Number of references by property")
-page.put(text, summary="upd", minorEdit=False)
+def write_number_of_references_by_property(dct:dict[str, int]) -> None:
+    write_report(dct, 'Template:Number of references by property')
+
 
 # write [[Wikidata:Database reports/List of properties/Top100]]
-header = 'A list of the top 100 [[Help:Properties|properties]] by quantity of item pages that link to them. Data as of <onlyinclude>{0}</onlyinclude>.\n\n{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"\n|-\n! Property !! Quantity of item pages\n'
-table_row = "|-\n| {{{{P|{0}}}}} || [//wikidata.org/wiki/Special:WhatLinksHere/Property:{0}?namespace=0 {1}]\n"
-footer = "|}\n\n[[Category:Properties]]\n[[Category:Wikidata statistics]]"
-sorted = sorted(total.items(), key=lambda item: item[1], reverse=True)
-content = ""
-for m in sorted[:100]:
-    content += table_row.format(m[0], m[1])
-text = header.format(time.strftime("%Y-%m-%d %H:%M (%Z)")) + content + footer
-page = pywikibot.Page(site, "Wikidata:Database reports/List of properties/Top100")
-page.put(text, summary="upd", minorEdit=False)
+def write_top100_database_report(total:dict[str, int]) -> None:
+    header = f"""A list of the top 100 [[Help:Properties|properties]] by quantity of item pages that link to them. Data as of <onlyinclude>{strftime("%Y-%m-%d %H:%M (%Z)")}</onlyinclude>.
+
+{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|-
+! Property !! Quantity of item pages
+"""
+
+    table_row = """|-
+| {{{{P|{pid}}}}} || [//wikidata.org/wiki/Special:WhatLinksHere/Property:{pid}?namespace=0 {txt}]
+"""
+
+    footer = """|}
+
+[[Category:Properties]]
+[[Category:Wikidata statistics]]"""
+
+    sorted_total = sorted(total.items(), key=lambda item: item[1], reverse=True)
+    content = ""
+    for m in sorted_total[:100]:
+        content += table_row.format(pid=m[0], txt=m[1])
+
+    wikitext = header + content + footer
+
+    save_to_wiki_page('Wikidata:Database reports/List of properties/Top100', wikitext)
+
+
+def main() -> None:
+    total, mainsnak, qualifiers, references = collect_data()
+
+    write_property_uses_template(total)
+    write_number_of_main_statements_by_property_template(mainsnak)
+    write_number_of_qualifiers_by_property_template(qualifiers)
+    write_number_of_references_by_property(references)
+    write_top100_database_report(total)
+
+
+if __name__=='__main__':
+    main()

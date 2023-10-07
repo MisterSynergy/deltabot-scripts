4449086bd263a2b001b09fe7302c8657350e2549
MisterSynergy <mister.synergy@yahoo.com>
2023-10-07 16:08:18 +0200

    refactor

--- vandal_stat/vandalstat.py
+++ vandal_stat/vandalstat.py
@@ -1,68 +1,80 @@
 # -*- coding: UTF-8 -*-
 
-from os.path import expanduser
-import mariadb
 from datetime import date, timedelta
-import pywikibot
+from os.path import expanduser
+
+import mariadb
+import pywikibot as pwb
 import requests
 
-site = pywikibot.Site('wikidata','wikidata')
-repo = site.data_repository()
 
-db = mariadb.connect(host="wikidatawiki.labsdb", database="wikidatawiki_p", default_file=f"{expanduser('~')}/replica.my.cnf")
+class Replica:
+    def __init__(self) -> None:
+        self.connection = mariadb.connect(
+            host='wikidatawiki.analytics.db.svc.wikimedia.cloud',
+            database='wikidatawiki_p',
+            default_file=f'{expanduser("~")}/replica.my.cnf',
+        )
+        self.cursor = self.connection.cursor(dictionary=True)
 
-r = requests.get('https://www.wikidata.org/w/index.php?title=Wikidata:WikiProject_Counter-Vandalism/plot1-csv&action=raw')
-csv = r.text.split('\n')
-newcsv = ''
+    def __enter__(self):
+        return self.cursor
 
-oldest = date.today() - timedelta(31)
-oldestformat = oldest.strftime('%Y%m%d')
-oldestformat2 = oldest.strftime('%Y/%m/%d')
-newest = date.today() - timedelta(1)
-newestformat = newest.strftime('%Y%m%d')
-newestformat2 = newest.strftime('%Y/%m/%d')
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.cursor.close()
+        self.connection.close()
 
 
-for line in csv:
-    if 'unpatrolled edits' in line or 'IP' in line:
-         continue
-    if oldestformat2 in line:
-         continue
-    newcsv += line.strip()+'\n'
-
-for dd in range(0,30):
-    day = date.today() - timedelta(30-dd)
-    dayformat = day.strftime('%Y%m%d')
-    dayformat2 = day.strftime('%Y/%m/%d')
-
-    cur = db.cursor(dictionary=True)
-    cur.execute(
-        'SELECT COUNT(*) as cnt FROM recentchanges WHERE rc_patrolled=0 AND rc_timestamp>%(dayformat_start)s AND rc_timestamp<=%(dayformat_end)s',
-        {
-            'dayformat_start' : f'{dayformat}000000',
-            'dayformat_end' : f'{dayformat}235959',
-        }
+def main() -> None:
+    response = requests.get(
+        url='https://www.wikidata.org/w/index.php?title=Wikidata:WikiProject_Counter-Vandalism/plot1-csv&action=raw'
     )
-    
-    for row in cur.fetchall():
-        newcsv += f'{dayformat2},{row.get("cnt", 0)},"unpatrolled edits"\n'
+    old_csv = response.text.split('\n')
+    new_csv = ''
 
-    cur.close()
+    oldest = date.today() - timedelta(31)
+    newest = date.today() - timedelta(1)
 
-cur = db.cursor(dictionary=True)
-cur.execute(
-    "SELECT COUNT(*) AS patrols FROM logging WHERE log_action='patrol' AND log_params LIKE '%\"6::auto\";i:0%' AND log_timestamp>%(newestformat_start)s AND log_timestamp<=%(newestformat_end)s",
-    {
-        'newestformat_start' : f'{newestformat}000000',
-        'newestformat_end' : f'{newestformat}235959',
+    for line in old_csv:
+        if 'unpatrolled edits' in line or 'IP' in line:
+            continue
+        if oldest.strftime('%Y/%m/%d') in line:
+            continue
+        new_csv += line.strip()+'\n'
+
+    for dd in range(0,30):
+        day = date.today() - timedelta(30-dd)
+
+        query = 'SELECT COUNT(*) as cnt FROM recentchanges WHERE rc_patrolled=0 AND rc_timestamp>%(dayformat_start)s AND rc_timestamp<=%(dayformat_end)s'
+        params = {
+            'dayformat_start' : f'{day.strftime("%Y%m%d")}000000',
+            'dayformat_end' : f'{day.strftime("%Y%m%d")}235959',
+        }
+
+        with Replica() as cur:
+            cur.execute(query, params)
+            result = cur.fetchall()
+
+        for row in result:
+            new_csv += f'{day.strftime("%Y/%m/%d")},{row.get("cnt", 0)},"unpatrolled edits"\n'
+
+    query = """SELECT COUNT(*) AS patrols FROM logging WHERE log_action='patrol' AND log_params LIKE '%\"6::auto\";i:0%' AND log_timestamp>%(newestformat_start)s AND log_timestamp<=%(newestformat_end)s"""
+    params = {
+        'newestformat_start' : f'{newest.strftime("%Y%m%d")}000000',
+        'newestformat_end' : f'{newest.strftime("%Y%m%d")}235959',
     }
-)
-for row in cur.fetchall():
-    newcsv += f'{newestformat2},{row.get("patrols", 0)},"patrol actions"\n' 
 
-cur.close()
-db.close()
+    with Replica() as cur:
+        cur.execute(query, params)
+        result = cur.fetchall()
 
-page = pywikibot.Page(site, 'Wikidata:WikiProject_Counter-Vandalism/plot1-csv')
-page.put(newcsv, summary='upd', minorEdit=False)
+    for row in result:
+        new_csv += f'{newest.strftime("%Y/%m/%d")},{row.get("patrols", 0)},"patrol actions"\n' 
 
+    page = pwb.Page(pwb.Site('wikidata', 'wikidata'), 'Wikidata:WikiProject_Counter-Vandalism/plot1-csv')
+    page.text = new_csv
+    page.save(summary='upd', minor=False)
+
+
+if __name__=='__main__':
+    main()

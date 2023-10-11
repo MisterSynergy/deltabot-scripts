f4123aa05d6dd7e91dd16faa3cd51f213c667d85
MisterSynergy <mister.synergy@yahoo.com>
2023-10-11 17:52:26 +0200

    refactor; job to be discontinued, though

--- property_list_by_cat/property_list_by_cat.py
+++ property_list_by_cat/property_list_by_cat.py
@@ -2,167 +2,244 @@
 # -*- coding: UTF-8 -*-
 #licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import pywikibot
-import requests
-import json
+from json.decoder import JSONDecodeError
+from typing import Any, Generator, Optional
+
+import pywikibot as pwb
 from pywikibot.exceptions import SpamblacklistError
-
-site = pywikibot.Site('wikidata', 'wikidata')
-repo = site.data_repository()
+import requests
 
 
-def getValue(datavalue, urlformatter):
-    if type(datavalue) == pywikibot.ItemPage or type(datavalue) == pywikibot.PropertyPage:
-        return datavalue.getID()
-    elif type(datavalue) == pywikibot.WbQuantity:
-        if datavalue.unit == '1':
-            return str(datavalue.amount)
-        else:
-            return str(datavalue.amount) + ' {{label|' + datavalue.unit[31:] + '}}'
-    elif type(datavalue) == pywikibot.FilePage:
-        return datavalue.title()[5:]
-    elif type(datavalue) == pywikibot.WbTime:
-        if datavalue.precision == 11:
-            return '{:04d}-{:02d}-{:02d}'.format(datavalue.year, datavalue.month, datavalue.day)
-        elif datavalue.precision == 10:
-            return '{:04d}-{:02d}'.format(datavalue.year, datavalue.month)
-        else:
-            return '{:04d}'.format(datavalue.year)
-    elif type(datavalue) == pywikibot.Coordinate:
-        return '{:7.4f}/{:7.4f}'.format(datavalue.lat, datavalue.lon)
-    elif type(datavalue) == pywikibot.WbMonolingualText:
-        return datavalue.text + '(' + datavalue.language + ')'
-    elif 'id'in datavalue:
-        return datavalue['id']
-    else:
-        if urlformatter:
-            datavalue = '[' + urlformatter.replace('$1', datavalue) + ' ' + datavalue + ']'
-        return str(datavalue)
+SITE = pwb.Site('wikidata', 'wikidata')
+REPO = SITE.data_repository()
 
-def createSection(q, title, rel):
-    text = u'{{List of properties/Header}}\n'
-    payload = {
-        'query': """SELECT DISTINCT ?prop ?datatype ?pair_num WHERE {{ ?prop {0} wd:{1}; wikibase:propertyType ?datatype
-                      OPTIONAL {{
-                        ?prop wdt:P1696 ?pair .
-                        BIND(substr(str(?pair), 33) as ?pair_num)
-                      }}
-                    }} ORDER BY xsd:integer(substr(str(?prop), 33))""".format(rel, q),
-        'format': 'json'
-    }    
-    r = requests.get('https://query.wikidata.org/bigdata/namespace/wdq/sparql?', params=payload)
+WDQS_ENDPOINT = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+WDQS_USER_AGENT = f'{requests.utils.default_user_agent()} (property_list_by_cat.py via User:DeltaBot at Wikidata; mailto:tools.deltabot@toolforge.org)'
+
+WD = 'http://www.wikidata.org/entity/'
+WIKIBASE = 'http://wikiba.se/ontology#'
+
+
+def query_wdqs(query) -> Generator[dict[str, Any], None, None]:
+    response = requests.post(
+        url=WDQS_ENDPOINT,
+        data={
+            'query' : query,
+        },
+        headers={
+            'Accept' : 'application/sparql-results+json',
+            'User-Agent' : WDQS_USER_AGENT,
+        }
+    )
+
     try:
-        data = r.json() 
-    except:
-        return ''
-    if len(data['results']['bindings']) == 0:
-        return ''
-    if len(data['results']['bindings']) > 1000 and title.count('/') <=1:
-        item = pywikibot.ItemPage(repo, q)
+        data = response.json() 
+    except JSONDecodeError as exception:
+        raise RuntimeError('Cannot parse response as JSON') from exception
+
+    for row in data.get('results', {}).get('bindings', []):
+        yield row
+
+
+def get_value(data_value:Any, url_formatter:Optional[str]) -> str:
+    if isinstance(data_value, pwb.ItemPage) or isinstance(data_value, pwb.PropertyPage):
+        return data_value.getID()
+
+    if isinstance(data_value, pwb.WbQuantity):
+        if data_value.unit == '1':
+            return str(data_value.amount)
+
+        return f'{data_value.amount} {{{{label|{data_value.unit[31:]}}}}}'
+
+    if isinstance(data_value, pwb.FilePage):
+        return data_value.title()[5:]
+
+    if isinstance(data_value, pwb.WbTime):
+        if data_value.precision == 11:
+            return f'{data_value.year:04d}-{data_value.month:02d}-{data_value.day:02d}'
+
+        if data_value.precision == 10:
+            return f'{data_value.year:04d}-{data_value.month:02d}'
+
+        return f'{data_value.year:04d}'
+
+    if isinstance(data_value, pwb.Coordinate):
+        return f'{data_value.lat:7.4f}/{data_value.lon:7.4f}'
+
+    if isinstance(data_value, pwb.WbMonolingualText):
+        return f'{data_value.text} ({data_value.language})'
+
+    if 'id' in data_value:
+        return data_value['id']
+
+    if url_formatter is not None:
+        data_value = f'[{url_formatter.replace("$1", data_value)} {data_value}]'
+
+    return str(data_value)
+
+
+def create_section(qid:str, page_title:str, property_path:str) -> Optional[str]:
+    text = '{{List of properties/Header}}\n'
+
+    query = f"""SELECT DISTINCT ?prop ?datatype ?pair_num WHERE {{
+        ?prop {property_path} wd:{qid}; wikibase:propertyType ?datatype .
+        OPTIONAL {{
+            ?prop wdt:P1696 ?pair .
+            BIND(SUBSTR(STR(?pair), 33) AS ?pair_num) .
+        }}
+    }} ORDER BY xsd:integer(SUBSTR(STR(?prop), 33))"""
+    data = list(query_wdqs(query))
+
+    if len(data) == 0:
+        return None
+
+    if len(data) > 1000 and page_title.count('/') <= 1:
+        item = pwb.ItemPage(REPO, qid)
         item.get()
+
         if 'P1269' in item.claims:
-            sublabel1 = item.claims['P1269'][0].getTarget().get()['labels']['en']
-            if sublabel1 in title and len(item.claims['P1269']) > 1:
-                title += '/' + item.claims['P1269'][1].getTarget().get()['labels']['en']
+            sublabel1 = item.claims['P1269'][0].getTarget().get()['labels']['en']  # TODO
+            if sublabel1 in page_title and len(item.claims['P1269']) > 1:
+                page_title += f'/{item.claims["P1269"][1].getTarget().get()["labels"]["en"]}'  # TODO
             else:
-                title += '/' + sublabel1
+                page_title += f'/{sublabel1}'
         else:
-            title += '/' + item.labels['en']
-        createCatPage(title, q)
-        return "''see [[Wikidata:List of properties/" + title + "]]''\n\n"
-        
-    for m in data['results']['bindings']:
-        pid = m['prop']['value'].replace('http://www.wikidata.org/entity/', '')
-        if pid[0] != 'P':
+            page_title += f'/{item.labels["en"]}'  # TODO
+
+        create_category_page(page_title, qid)
+
+        return f"''see [[Wikidata:List of properties/{page_title}]]''\n\n"
+
+    for row in data:
+        pid = row.get('prop', {}).get('value', '').replace(WD, '')
+        if not pid.startswith('P'):
             continue
+
         if pid in ['P5267', 'P5540']:
             continue # Wikimedia spam black list
-        datatype = m['datatype']['value'][26:].lower()
-        pair = m['pair_num']['value'] if 'pair_num' in m else ''
-        proppage = pywikibot.PropertyPage(repo, pid)
-        proppage.get()
+
+        datatype = row.get('datatype', {}).get('value', '').replace(WIKIBASE, '').lower()
+        pair = row.get('pair_num', {}).get('value', '')
+
+        property_page = pwb.PropertyPage(REPO, pid)
+        property_page.get()
         try:
-            subject = proppage.claims['P1855'][0].getTarget().getID()
-            object = proppage.claims['P1855'][0].qualifiers[pid][0].getTarget()
-            if 'P1630' in proppage.claims:
-                urlformatter = proppage.claims['P1630'][0].getTarget()
+            if 'P1855' in property_page.claims:
+                subject = property_page.claims['P1855'][0].getTarget().getID()  # TODO
+                obj = property_page.claims['P1855'][0].qualifiers[pid][0].getTarget()  # TODO
             else:
-                urlformatter = False
-            objectvalue = getValue(object, urlformatter)
-            text += '{{{{List of properties/Row|id={0}|example-subject={1}|example-object={2}|pair={3}|datatype={4}|noexpensivecalls=1}}}}\n'.format(pid[1:], subject, objectvalue, pair, datatype)
-        except:
-            text += '{{{{List of properties/Row|id={0}|pair={1}|datatype={2}|noexpensivecalls=1}}}}\n'.format(pid[1:], pair, datatype)
+                subject, obj = '', ''  # TODO
+
+            if 'P1630' in property_page.claims:
+                url_formatter = property_page.claims['P1630'][0].getTarget()  # TODO
+            else:
+                url_formatter = None
+
+            objectvalue = get_value(obj, url_formatter)
+            text += f'{{{{List of properties/Row|id={pid[1:]}|example-subject={subject}|example-object={objectvalue}|pair={pair}|datatype={datatype}|noexpensivecalls=1}}}}\n'
+        except:  # TODO
+            text += f'{{{{List of properties/Row|id={pid[1:]}|pair={pair}|datatype={datatype}|noexpensivecalls=1}}}}\n'
+
     text += '{{Template:List_of_properties/Footer}}\n\n'
+
     return text
-                
-def createCatPage(title, q):
-    print(title)
-    payload = {
-        'query': 'SELECT ?item WHERE {{ ?item wdt:P279 wd:{0}}} ORDER BY xsd:integer(substr(str(?item), 33))'.format(q),
-        'format': 'json'
-    }
-    r = requests.get('https://query.wikidata.org/bigdata/namespace/wdq/sparql?', params=payload)
-    data = r.json()    
-    text = u'{{{{Q|{0}}}}}\n'.format(q)
-    if len(data['results']['bindings']) > 0:
-        for m in data['results']['bindings']:
-            qsec = m['item']['value'].replace('http://www.wikidata.org/entity/', '')
-            text += u'=== {{{{label|{0}}}}} ===\n'.format(qsec)
-            text += createSection(qsec, title, 'wdt:P31/wdt:P279*')
-        other = createSection(q, title, 'wdt:P31')
-        if other != '':
-            text += u'=== Other ===\n' + other
+
+
+def create_category_page(page_title:str, qid:str) -> None:
+    print(page_title)
+
+    text = f'{{{{Q|{qid}}}}}\n'
+
+    query = f'SELECT ?item WHERE {{ ?item wdt:P279 wd:{qid}}} ORDER BY xsd:integer(SUBSTR(STR(?item), STRLEN(STR(wd:))+2))'
+    data = list(query_wdqs(query))
+
+    if len(data) > 0:  # qid has subclasses
+        for row in data:
+            qid_section = row.get('item', {}).get('value', '').replace(WD, '')
+
+            section_text = create_section(qid_section, page_title, 'wdt:P31/wdt:P279*')
+            if section_text is not None:
+                text += f'=== {{{{label|{qid_section}}}}} ===\n{section_text}'
+
+        other_text = create_section(qid, page_title, 'wdt:P31')
+        if other_text is not None:
+            text += f'=== Other ===\n{other_text}'
+
     else:
-        text += createSection(q, title, 'wdt:P31')
-    text += u'[[Category:Wikidata:List of properties|' + title + ']]'
-    page = pywikibot.Page(site, 'Wikidata:List of properties/' + title)
+        other_text = create_section(qid, page_title, 'wdt:P31')
+        if other_text is not None:
+            text += other_text
+
+    text += f'[[Category:Wikidata:List of properties|{page_title}]]'
+
+    page = pwb.Page(SITE, f'Wikidata:List of properties/{page_title}')
+    page.text = text
     try:
-        page.put(text, summary='upd', minorEdit=False)
+        page.save(summary='upd', minor=False)
     except SpamblacklistError:
-        pass
+        print(f'Cannot save "Wikidata:List of properties/{page_title}" due to spam blacklist issue')
+        # do not do anything else at this point
 
 
-def createOverview():
-    payload = {
-        'query': """SELECT ?item (GROUP_CONCAT(?qfacet; separator=", ") as ?facets) (GROUP_CONCAT(?label; separator=", ") as ?labels){
-                      ?item wdt:P279 wd:Q18616576
-                      OPTIONAL { ?item wdt:P1269 ?facet . ?facet rdfs:label ?facetlabel . FILTER (LANG(?facetlabel) = 'en') }
-                      BIND(IF(bound(?facet), substr(str(?facet), 32), substr(str(?item), 32)) AS ?qfacet)
-                      ?item rdfs:label ?itemlabel . FILTER(LANG(?itemlabel) = 'en')
-                      BIND(IF(bound(?facet), str(?facetlabel), str(?itemlabel)) AS ?label)
-    }GROUP BY ?item ORDER BY fn:lower-case(?labels)""",
-        'format': 'json'
-    }
-    r = requests.get('https://query.wikidata.org/bigdata/namespace/wdq/sparql?', params=payload)
-    data = r.json()
-    cat1st = {}
-    text = u''
+def create_overview() -> dict[str, str]:
+    query = """SELECT ?item (GROUP_CONCAT(?qfacet; SEPARATOR=', ') AS ?facets) (GROUP_CONCAT(?label; SEPARATOR=', ') AS ?labels) WHERE {
+        ?item wdt:P279 wd:Q18616576 .
+        OPTIONAL {
+            ?item wdt:P1269 ?facet .
+            ?facet rdfs:label ?facetlabel .
+            FILTER (LANG(?facetlabel) = 'en') .
+        }
+        BIND(IF(BOUND(?facet), SUBSTR(STR(?facet), 32), SUBSTR(STR(?item), 32)) AS ?qfacet) .
+        ?item rdfs:label ?itemlabel .
+        FILTER(LANG(?itemlabel) = 'en') .
+        BIND(IF(BOUND(?facet), STR(?facetlabel), STR(?itemlabel)) AS ?label) .
+    } GROUP BY ?item ORDER BY fn:lower-case(?labels)"""
+
+    query_wdqs(query)
+    
+    cat1st:dict[str, str] = {}
+    text = ''
     i = 0
-    for m in data['results']['bindings']:
-        q = m['item']['value'].replace('http://www.wikidata.org/entity/', '')
-        link = m['labels']['value'].split(',')[0]
+    for row in query_wdqs(query):
+        qid = row.get('item', {}).get('value', '').replace(WD, '')
+        link = row.get('labels', {}).get('value', '').split(',')[0]
+
         if ':' in link:
-            link = m['labels']['value'].split(':')[1]
-        if link in cat1st:
-            link += ' (2)'
-            if link in cat1st:
-                link += ' (3)'
-        cat1st[link] = q
-        label = m['facets']['value'].replace('Q', '{{label|Q').replace(',', '}},') + '}}'
-        if i % 5 == 0:
-            text += u'|-\n'
-        text += u"""|style="background-color:#eee; box-shadow: 0 0 .2em #999; border-radius: .2em; padding: 20px; width:20%; font-size:105%;"|\n'''[[Wikidata:List of properties/{0}|{1}]]'''\n""".format(link, label)
+            link = row.get('labels', {}).get('value', '').split(':')[1]
+
+        n = 2
+        while link in cat1st:
+            link = f'{link} ({n})'
+            n += 1
+
+        cat1st[link] = qid
+
+        facets = row.get('facets', {}).get('value')
+        if facets is None or len(facets) == 0:
+            label = ''
+        else:
+            label = f'{{{{{"}}, {{label|Q}}".join(facets.split(", "))}}}}}'
+
+        if i % 5 == 0:  # five columns on https://www.wikidata.org/wiki/Wikidata:List_of_properties
+            text += '|-\n'
+        text += f"""| style="background-color:#eee; box-shadow: 0 0 .2em #999; border-radius: .2em; padding: 20px; width:20%; font-size:105%;" |
+'''[[Wikidata:List of properties/{link}|{label}]]'''
+"""
+
         i += 1
 
-    page = pywikibot.Page(site, 'Wikidata:List of properties/cat overview')
-    page.put(text, summary='upd', minorEdit=False)     
-    
+    page = pwb.Page(SITE, 'Wikidata:List of properties/cat overview')  # transcluded by https://www.wikidata.org/wiki/Wikidata:List_of_properties
+    page.text = text
+    page.save(summary='upd', minor=False)     
+
     return cat1st
-    
-def main():
-    cat1st = createOverview()
+
+
+def main() -> None:
+    cat1st = create_overview()
     for cat in cat1st:
-        createCatPage(cat, cat1st[cat])
-    
-if __name__ == "__main__":
+        create_category_page(cat, cat1st[cat])
+
+
+if __name__=='__main__':
     main()    

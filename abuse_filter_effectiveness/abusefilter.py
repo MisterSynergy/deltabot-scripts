04b2cafaa3a1508b2a1f67dc068bf6b9e59b4243
MisterSynergy <mister.synergy@yahoo.com>
2023-10-12 11:08:39 +0200

    refactor; migrate to mariadb connector

--- abuse_filter_effectiveness/abusefilter.py
+++ abuse_filter_effectiveness/abusefilter.py
@@ -2,49 +2,105 @@
 # -*- coding: UTF-8 -*-
 #licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import MySQLdb
-import pywikibot
-import time
+from datetime import datetime
+from os.path import expanduser
+from time import strftime
 
-site = pywikibot.Site('wikidata','wikidata')
+import mariadb
+import pywikibot as pwb
 
-header = 'Update: <onlyinclude>{0}</onlyinclude>\n\n{{| class="wikitable sortable plainlinks"\n|-\n!Id !! Description !! Active Since !! Just Warned !! Edited !! Warning Deterred\n'
-footer = '|}\n\n[[Category:Wikidata statistics]]'
 
-table_row = '|-\n| [//wikidata.org/wiki/Special:AbuseLog?wpSearchFilter={0} {0}] || {1} || {2}-{3}-{4} || {5} || {6} || {7}%\n'
+HEADER = """Update: <onlyinclude>{update_timestamp}</onlyinclude>
 
-query1 = 'SELECT af_id, af_public_comments FROM abuse_filter WHERE af_actions="warn,tag"'
-query2 = 'SELECT afh_timestamp FROM abuse_filter_history WHERE afh_changed_fields LIKE "%actions%" AND afh_filter={0} ORDER BY afh_timestamp DESC LIMIT 1'
-query3 = 'SELECT count(*) FROM abuse_filter_log WHERE afl_filter={0} AND afl_actions="{1}" AND afl_timestamp>{2}'
+{{| class="wikitable sortable plainlinks"
+|-
+!Id !! Description !! Active Since !! Just Warned !! Edited !! Warning Deterred
+"""
 
-def makeReport(db):
+FOOTER = """|}
+
+[[Category:Wikidata statistics]]"""
+
+TABLE_ROW = """|-
+| [//wikidata.org/wiki/Special:AbuseLog?wpSearchFilter={af_id} {af_id}] || {title} || {start_date} || {warned} || {edited} || {deterred}%
+"""
+
+TS_FORMAT_MW = '%Y%m%d%H%M%S'
+
+
+def make_report() -> str:
+    query1 = 'SELECT af_id, CONVERT(af_public_comments USING utf8) AS af_public_comments FROM abuse_filter WHERE af_actions="warn,tag"'
+    query2 = 'SELECT CONVERT(afh_timestamp USING utf8) AS afh_timestamp FROM abuse_filter_history WHERE afh_changed_fields LIKE "%actions%" AND afh_filter=%(afh_filter)s ORDER BY afh_timestamp DESC LIMIT 1'
+    query3 = 'SELECT COUNT(*) AS cnt FROM abuse_filter_log WHERE afl_filter_id=%(afl_filter)s AND afl_actions=%(afl_actions)s AND afl_timestamp>%(afl_timestamp)s'
+
+    db = mariadb.connect(
+        host='wikidatawiki.analytics.db.svc.wikimedia.cloud',
+        database='wikidatawiki_p',
+        default_file=f'{expanduser("~")}//replica.my.cnf',
+    )
+    cur = db.cursor(dictionary=True)
+
+    cur.execute(query1)
+    result1 = cur.fetchall()
+
     text = ''
-    cursor1 = db.cursor()
-    cursor1.execute(query1)
-    for af, comment in cursor1:
-        cursor2 = db.cursor()
-        af = int(af)
-        cursor2.execute(query2.format(af))
-        start = cursor2.fetchall()[0][0]
-        cursor3 = db.cursor()
-        cursor3.execute(query3.format(af,'warn',start))
-        warn = cursor3.fetchall()[0][0]
-        cursor4 = db.cursor()
-        cursor4.execute(query3.format(af,'tag',start))
-        tag = cursor4.fetchall()[0][0]
-        if warn != 0:
-            deterred = str(round((warn-tag)/float(warn)*100,1))
+    for row in result1:
+        af_id = row.get('af_id')
+        comment = row.get('af_public_comments')
+        if af_id is None or comment is None:
+            continue
+
+        cur.execute(query2, { 'afh_filter' : af_id })
+        result2 = cur.fetchall()
+        if len(result2) == 0:
+            continue
+
+        start = result2[0].get('afh_timestamp')
+        if start is None:
+            continue
+        start_date_ts = datetime.strptime(start, TS_FORMAT_MW)
+
+        cur.execute(query3, { 'afl_filter' : af_id, 'afl_actions' : 'warn', 'afl_timestamp' : start_date_ts.strftime(TS_FORMAT_MW) })
+        result3a = cur.fetchall()
+        warn = result3a[0].get('cnt')
+        if warn is None:
+            continue
+
+        cur.execute(query3, { 'afl_filter' : af_id, 'afl_actions' : 'tag', 'afl_timestamp' : start_date_ts.strftime(TS_FORMAT_MW) })
+        result3b = cur.fetchall()
+        tag = result3b[0].get('cnt')
+        if tag is None:
+            continue
+
+        if warn > 0:
+            deterred = f'{(warn-tag)/warn*100:.1f}'
         else:
             deterred = '0'
-        text += table_row.format(af,comment,start[0:4],start[4:6],start[6:8],warn-tag,tag,deterred)
+
+        text += TABLE_ROW.format(
+            af_id=af_id,
+            title=comment,
+            start_date=start_date_ts.strftime('%Y-%m-%d'),
+            warned=warn-tag,
+            edited=tag,
+            deterred=deterred,
+        )
+
+    cur.close()
+    db.close()
+
     return text
 
-def main():
-    page = pywikibot.Page(site,'Wikidata:Database reports/Abuse filter effectiveness')
-    db = MySQLdb.connect(host="wikidatawiki.labsdb",db="wikidatawiki_p", read_default_file="~/replica.my.cnf")
-    report = makeReport(db)
-    text = header.format(time.strftime("%Y-%m-%d %H:%M (%Z)")) + report + footer
-    page.put(text.decode('UTF-8'),comment='Bot:Updating database report',minorEdit=False)
 
-if __name__ == "__main__":
+def main() -> None:
+    text = HEADER.format(update_timestamp=strftime('%Y-%m-%d %H:%M (%Z)')) + make_report() + FOOTER
+
+    page = pwb.Page(pwb.Site('wikidata', 'wikidata'), 'Wikidata:Database reports/Abuse filter effectiveness')
+    page.text = text
+
+    print(text)
+    #page.save(summary='Bot:Updating database report', minor=False)
+
+
+if __name__=='__main__':
     main()

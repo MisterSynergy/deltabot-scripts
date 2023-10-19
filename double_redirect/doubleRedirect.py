ef1ff3404c1225354d0cf7f25eb646237a5a43d7
MisterSynergy <mister.synergy@yahoo.com>
2023-10-19 10:32:18 +0200

    allow Lexemes to be resolved as well

--- double_redirect/doubleRedirect.py
+++ double_redirect/doubleRedirect.py
@@ -2,52 +2,90 @@
 # -*- coding: UTF-8 -*-
 # licensed under CC-Zero: https://creativecommons.org/publicdomain/zero/1.0
 
-import pywikibot
-from pywikibot.data import api
 import re
+from time import sleep
+from typing import Any
 
-site = pywikibot.Site('wikidata', 'wikidata')
-site.login()
-repo = site.data_repository()
+import pywikibot as pwb
+from pywikibot.data import api
+from pywikibot.exceptions import IsNotRedirectPageError
 
 
-def redirect(fromId, toId):
-    # get token
+SITE = pwb.Site('wikidata', 'wikidata')
+REPO = SITE.data_repository()
+
+
+def get_token() -> str:
     params = {
-        'action': 'query',
-        'meta': 'tokens'
+        'action' : 'query',
+        'meta' : 'tokens',
     }
-    req = api.Request(site=site, parameters=params)
-    data = req.submit()
-    # create redirect
-    params3 = {
-        'action': 'wbcreateredirect',
-        'from': fromId,
-        'to': toId,
-        'bot': 1,
-        'token': data['query']['tokens']['csrftoken']
-    }
-    req3 = api.Request(site=site, parameters=params3)
-    data3 = req3.submit()
+    request = api.Request(SITE, parameters=params)
+    data = request.submit()
+
+    token = data.get('query', {}).get('tokens', {}).get('csrftoken')
+    if token is None:
+        raise RuntimeError('No valid token obtained')
+
+    return token
 
 
-def main():
+def redirect(from_id:str, to_id:str) -> None:
+    token = get_token()
+
     params = {
-        'action': 'query',
-        'list': 'querypage',
-        'qppage': 'DoubleRedirects',
-        'qplimit': 5000
+        'action' : 'wbcreateredirect',
+        'from' : from_id,
+        'to' : to_id,
+        'bot' : '1',
+        'token' : token,
     }
-    req = api.Request(site=site, parameters=params)
-    data = req.submit()
-    for m in data['query']['querypage']['results']:
-        try:
-            if m['ns'] == 0:
-                item1 = pywikibot.ItemPage(repo, m['title'])
-                item2 = item1.getRedirectTarget().getRedirectTarget().getID()
-                redirect(m['title'], item2)
-        except:
-            pass
+    request = api.Request(SITE, parameters=params)
+    _ = request.submit()
 
-if __name__ == "__main__":
+
+def get_double_redirects() -> list[dict[str, Any]]:
+    #https://www.wikidata.org/w/api.php?action=query&list=querypage&qppage=DoubleRedirects&qplimit=500
+    params = {
+        'action' : 'query',
+        'list' : 'querypage',
+        'qppage' : 'DoubleRedirects',
+        'qplimit' : '500',
+    }
+    request = api.Request(SITE, parameters=params)
+    data = request.submit()
+
+    return data.get('query', {}).get('querypage', {}).get('results', [])
+
+
+def main() -> None:
+    for row in get_double_redirects():
+        ns = row.get('ns')
+        page_title = row.get('title')
+        if ns is None or page_title is None:
+            continue
+
+        if ns not in [ 0, 146 ]:
+            continue
+
+        if ns == 0 and re.match(r'^Q[\d]+$', page_title) is not None:
+            entity = pwb.ItemPage(REPO, page_title)
+            try:
+                target_qid = entity.getRedirectTarget().getRedirectTarget().getID()
+            except IsNotRedirectPageError as exception:
+                continue
+            else:
+                redirect(page_title, target_qid)
+
+        elif ns == 146 and re.match(r'^Lexeme:L[\d]+$', page_title) is not None:
+            entity = pwb.LexemePage(REPO, page_title)
+            try:
+                target_lid = entity.getRedirectTarget().getRedirectTarget().title()
+            except IsNotRedirectPageError as exception:
+                continue
+            else:
+                redirect(page_title.replace('Lexeme:', ''), target_lid.replace('Lexeme:', ''))
+
+
+if __name__=='__main__':
     main()

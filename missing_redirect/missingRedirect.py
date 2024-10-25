92e21abb018f6f24c7f5df0cfd041f937e42eafa
MisterSynergy <mister.synergy@yahoo.com>
2024-10-25 20:01:14 +0200

    refactor: functions, type hints, dict.get, coding style

--- missing_redirect/missingRedirect.py
+++ missing_redirect/missingRedirect.py
@@ -5,6 +5,7 @@
 from datetime import datetime, timedelta
 from pathlib import Path
 import re
+from typing import Any, Optional
 
 import pywikibot
 from pywikibot.data import api
@@ -14,85 +15,109 @@ SITE = pywikibot.Site('wikidata', 'wikidata')
 SITE.login()
 REPO = SITE.data_repository()
 
-cat = pywikibot.Category(SITE, 'Category:Notability policy exemptions')
-liste = list(cat.articles(recurse=5))
+EXEMPTION_CATEGORY = pywikibot.Category(SITE, 'Category:Notability policy exemptions')
+EXEMPTION_LIST = list(EXEMPTION_CATEGORY.articles(recurse=5))
+
+TIME_FILE = Path.home() / 'jobs/missing_redirect/missingRedirect_time.dat'
 
 
-def merge(fromId,toId):
-    fromItem = pywikibot.ItemPage(REPO,fromId)
-    toItem = pywikibot.ItemPage(REPO,toId)
+def merge(from_id:str, to_id:str) -> None:
+    fromItem = pywikibot.ItemPage(REPO, from_id)
+    toItem = pywikibot.ItemPage(REPO, to_id)
     fromItem.mergeInto(toItem, ignore_conflicts='description')
-    clearItem(fromId)
+    clear_item(from_id)
     fromItem.set_redirect_target(toItem, force=True, save=True)
 
-def clearItem(fromId):
-    #get token
-    params = {
+
+def clear_item(from_id:str) -> None:
+    token_params = {
         'action': 'query',
         'meta': 'tokens'
     }
-    req = api.Request(site=SITE, parameters=params)
-    data = req.submit()
-    #clear item
-    params2 = {
-        'action': 'wbeditentity',
-        'id': fromId,
-        'clear': 1,
-        'data':'{}',
-        'bot': 1,
-        'summary': 'clearing item to prepare for redirect',
-        'token': data['query']['tokens']['csrftoken']
+    token_request = api.Request(site=SITE, parameters=token_params)
+    token_data = token_request.submit()
+
+    params = {
+        'action' : 'wbeditentity',
+        'id' : from_id,
+        'clear' : 1,
+        'data' : '{}',
+        'bot' : 1,
+        'summary' : 'clearing item to prepare for redirect',
+        'token' : token_data.get('query', {}).get('tokens', {}).get('csrftoken', '')
     }
-    req2 = api.Request(site=SITE, parameters=params2)
-    data2 = req2.submit()
+    request = api.Request(site=SITE, parameters=params)
+    request.submit()
+
+
+def process_revision(revision:dict[str, Any]) -> Optional[str]:
+    processed_timestamp = revision.get('timestamp')
+    revision_comment = revision.get('comment')
+    if revision_comment is None:
+        return processed_timestamp
+
+    res = re.search('\/\* wbmergeitems-to:0\|\|(Q[0-9]+) \*\/', revision_comment)
+    if not res:
+        return processed_timestamp
+
+    #ignore items in Category:Notability policy exemptions
+    for list_entry in EXEMPTION_LIST:
+        if revision.get('title') == list_entry.title()[5:]:  # Talk: pages are categorized here
+            return processed_timestamp
+
+    try:
+        merge(revision['title'], res.group(1))
+    except Exception as exception:  # TODO
+        print(exception)
+
+    return processed_timestamp
+
+
+def make_api_request(old_time:str, new_time:str, rccontinue:str) -> tuple[Optional[str], Optional[str]]:
+    params = {
+        'action' : 'query',
+        'list' : 'recentchanges',
+        'rcprop' : 'title|comment|timestamp',
+        'rcstart' : old_time,
+        'rcend' : new_time,
+        'rcdir' : 'newer',
+        'rclimit' : 500,
+        'rctype' : 'edit',
+        'rcnamespace' : 0,
+        'rctoponly' : 1,
+        'rccontinue' : rccontinue,
+    }
+    request = api.Request(site=SITE, parameters=params)
+    data = request.submit()
+
+    processed_timestamp = None
+    for revision in data.get('query', {}).get('recentchanges', {}):
+        processed_timestamp = process_revision(revision)
+
+    rccontinue = data.get('query-continue', {}).get('recentchanges', {}).get('rccontinue')
+    return rccontinue, processed_timestamp
+
 
 def main():
-    time_file = Path.home() / 'jobs/missing_redirect/missingRedirect_time.dat'
-    with open(time_file,'r') as file_handle:
+    with open(TIME_FILE, 'r') as file_handle:
         done_until_str = file_handle.read()
-    
-    old_time_str = (datetime.strptime(done_until_str, "%Y%m%d%H%M%S") \
-        + timedelta(seconds=1)).strftime("%Y%m%d%H%M%S")
-    
+
+    old_time_str = (datetime.strptime(done_until_str, '%Y%m%d%H%M%S') \
+        + timedelta(seconds=1)).strftime('%Y%m%d%H%M%S')
+
     now = datetime.now()
     new_time = now - timedelta(minutes=10)
-    new_time_str = new_time.strftime("%Y%m%d%H%M%S")
+    new_time_str = new_time.strftime('%Y%m%d%H%M%S')
     rccontinue = old_time_str+'|0'
     while True:
-        params = {
-            'action': 'query',
-            'list': 'recentchanges',
-            'rcprop': 'title|comment|timestamp',
-            'rcstart': old_time_str,
-            'rcend': new_time_str,
-            'rcdir': 'newer',
-            'rclimit' : 500,
-            'rctype': 'edit',
-            'rcnamespace':0,
-            'rctoponly':1,
-            'rccontinue':rccontinue
-        }
-        req = api.Request(site=SITE, parameters=params)
-        data = req.submit()
-        for m in data['query']['recentchanges']:
-            timestamp = m['timestamp']
-            if 'comment' in m:
-                res = re.search('\/\* wbmergeitems-to:0\|\|(Q[0-9]+) \*\/', m['comment'])
-                if res:
-                    #ignore items in Category:Notability policy exemptions
-                    for ll in liste:
-                        if m['title'] == ll.title()[5:]:
-                            continue
-                    try:
-                        merge(m['title'],res.group(1))
-                    except:
-                        pass
-        if 'query-continue' in data:
-            rccontinue = data['query-continue']['recentchanges']['rccontinue']
-        else:
+        rccontinue, timestamp = make_api_request(old_time_str, new_time_str, rccontinue)
+        if rccontinue is None:
             break
-    with open(time_file, 'w') as file_handle:
-        file_handle.write(re.sub(r'\:|\-|Z|T', '', timestamp))
+
+    if timestamp is not None:
+        with open(TIME_FILE, 'w') as file_handle:
+            file_handle.write(re.sub(r'\:|\-|Z|T', '', timestamp))
+
 
 if __name__ == '__main__':
     main()
